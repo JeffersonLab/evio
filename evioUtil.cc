9d750e9fb7e94e02665ecf9afc5a16ec8442786f
Elliot Wolin <wolin@jlab.org>
2005-09-02 20:11:36 +0000

    DOM stuff mostly working

--- evioUtil.cc
+++ evioUtil.cc
@@ -10,6 +10,10 @@
 #include <evioUtil.hxx>
 
 
+// misc
+static bool debug = true;
+
+
 //--------------------------------------------------------------
 //--------------------------------------------------------------
 
@@ -215,5 +219,314 @@ string evioException::toString(void) const {
 }
 
 
-//--------------------------------------------------------------
-//--------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+
+class DOMHandler:public evioStreamHandler {
+
+
+  void *nodeHandler(int length, int ftype, int tag, int type, int num, int depth, 
+                    void *userArg) {
+    
+    if(debug) printf("node   depth %2d  ftype %3d   type,tag,num,length:  %6d %6d %6d %6d\n",
+                     depth,ftype,type,tag,num,length);
+    
+    
+    // get parent pointer
+    evioDOMContainerNode *parent = (evioDOMContainerNode*)userArg;
+    
+
+    // create new node
+    evioDOMContainerNode *newNode = new evioDOMContainerNode(ftype,tag,type,num);
+
+
+    // add new node to parent's list
+    if(parent!=NULL)parent->childList.push_back(newNode);
+
+
+    // return new userArg that points to the new node
+    return((void*)newNode);
+  }
+  
+  
+//-----------------------------------------------------------------------------
+
+
+  void leafHandler(int length, int ftype, int tag, int type, int num, int depth, 
+                   const void *data, void *userArg) {
+
+    if(debug) printf("leaf   depth %2d  ftype %3d   type,tag,num,length:  %6d %6d %6d %6d\n",
+                     depth,ftype,type,tag,num,length);
+
+
+    // get parent pointer
+    evioDOMContainerNode *parent = (evioDOMContainerNode*)userArg;
+
+
+    // create and fill new leaf
+    evioDOMNode *newLeaf;
+    string s;
+    ostringstream os;
+    switch (type) {
+
+    case 0x0:
+      newLeaf = new evioDOMLeafNode<unsigned long>(ftype,tag,type,num,(unsigned long *)data,length);
+      break;
+      
+    case 0x1:
+      newLeaf = new evioDOMLeafNode<unsigned long>(ftype,tag,type,num,(unsigned long*)data,length);
+      break;
+      
+    case 0x2:
+      newLeaf = new evioDOMLeafNode<float>(ftype,tag,type,num,(float*)data,length);
+      break;
+      
+    case 0x3:
+      for(int i=0; i<length; i++) os << ((char *)data)[i];
+      os << ends;
+      s=os.str();
+      newLeaf = new evioDOMLeafNode<string>(ftype,tag,type,num,&s,1);
+      break;
+
+    case 0x4:
+      newLeaf = new evioDOMLeafNode<short>(ftype,tag,type,num,(short*)data,length);
+      break;
+
+    case 0x5:
+      newLeaf = new evioDOMLeafNode<unsigned short>(ftype,tag,type,num,(unsigned short*)data,length);
+      break;
+
+    case 0x6:
+      newLeaf = new evioDOMLeafNode<char>(ftype,tag,type,num,(char*)data,length);
+      break;
+
+    case 0x7:
+      newLeaf = new evioDOMLeafNode<unsigned char>(ftype,tag,type,num,(unsigned char*)data,length);
+      break;
+
+    case 0x8:
+      newLeaf = new evioDOMLeafNode<double>(ftype,tag,type,num,(double*)data,length);
+      break;
+
+    case 0x9:
+      newLeaf = new evioDOMLeafNode<long long>(ftype,tag,type,num,(long long*)data,length);
+      break;
+
+    case 0xa:
+      newLeaf = new evioDOMLeafNode<unsigned long long>(ftype,tag,type,num,(unsigned long long*)data,length);
+      break;
+
+    case 0xb:
+      newLeaf = new evioDOMLeafNode<long>(ftype,tag,type,num,(long*)data,length);
+      break;
+    }
+
+
+    // add new leaf to parent list
+    if(parent!=NULL)parent->childList.push_back(newLeaf);
+  }
+
+};
+
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+
+evioDOMNode *evioDOMParser::parse(const unsigned long *buf) throw(evioException*) {
+  evioStreamParser p;
+  DOMHandler dh;
+  return((evioDOMNode*)p.parse(buf,dh,NULL));
+}
+
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+
+evioDOMContainerNode::evioDOMContainerNode(int container, int tg, int content, int n)
+  throw(evioException*) {
+
+  containerType = container;
+  tag           = tg;
+  contentType   = content;
+  num           = n;
+  
+}
+
+
+//-----------------------------------------------------------------------------
+
+
+string evioDOMContainerNode::toString(void) const {
+
+  // just dump header
+  ostringstream os;
+  os <<  "Bank:  containerType " << setw(3) << containerType << "    tag " << setw(6) << tag 
+     << "    contentType " << setw(6) << contentType 
+     << "    num " << setw(6) << num;
+  return(os.str());
+}
+
+
+//-----------------------------------------------------------------------------
+
+                                   
+void evioDOMContainerNode::toString(ostream &os, int depth) const {
+
+  string indent;
+  for(int i=0; i<depth; i++) indent +="    ";
+
+  os << "(" <<  depth << ")   " << indent << toString() << endl;
+}
+
+
+//-----------------------------------------------------------------------------
+
+                                   
+evioDOMContainerNode::~evioDOMContainerNode(void) {
+  if(debug)cout << "deleting node" << endl;
+  list<evioDOMNode*>::const_iterator iter;
+  for (iter=childList.begin(); iter!=childList.end(); iter++) {
+    if(debug)cout << "deleting child" << endl;
+    delete(*iter);
+  }
+}
+
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+                                   
+template <class T> evioDOMLeafNode<T>::evioDOMLeafNode(int container, int tg, int content, int n,
+                                                       T *p, int ndata) throw(evioException*) {
+  
+  containerType = container;
+  tag           = tg;
+  contentType   = content;
+  num           = n;
+
+  // fill vector with data
+  for(int i=0; i<ndata; i++) data.push_back(p[i]);
+
+}
+
+
+//-----------------------------------------------------------------------------
+
+
+template <class T> string evioDOMLeafNode<T>::toString(void) const {
+
+  ostringstream os;
+
+
+  // dump header
+  os <<  "Leaf:  containerType " << setw(3) << containerType << "    tag " << setw(6) << tag 
+     << "    contentType " << setw(6) << contentType 
+     << "    num " << setw(6) << num << "     data: ";
+
+
+  // dump data (odd what has to be done for type 0x7 or unsigned char...)
+  T i;
+  typename vector<T>::const_iterator iter;
+  for (iter=data.begin(); iter!=data.end(); iter++) {
+    switch (contentType) {
+    case 0x0:
+    case 0x1:
+    case 0x5:
+    case 0xa:
+      os << "0x" << hex << *iter << "  ";
+      break;
+    case 0x7:
+      i=*iter;
+      os << "0x" << hex << ((*(int*)&i)&0xff) << "  ";
+      break;
+    default:
+      os << *iter << "  ";
+      break;
+    }
+  }
+
+
+  return(os.str());
+}
+
+
+//-----------------------------------------------------------------------------
+
+                                   
+template <class T> void evioDOMLeafNode<T>::toString(ostream &os, int depth) const {
+
+  string indent;
+  for(int i=0; i<depth; i++) indent +="    ";
+
+  os << "(" <<  depth << ")   " << indent << toString() << endl;
+}
+
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+
+evioDOMTree::evioDOMTree(evioDOMNode *r) throw (evioException*) {
+  root = r;
+}
+
+
+//-----------------------------------------------------------------------------
+
+
+evioDOMTree::evioDOMTree(const unsigned long *buf) throw (evioException*) {
+  evioDOMParser p;
+  root = p.parse(buf);
+}
+
+
+//-----------------------------------------------------------------------------
+
+
+evioDOMTree::~evioDOMTree(void) {
+  delete(root);
+  if(debug)cout << "tree deleted" << endl;
+}
+
+
+//-----------------------------------------------------------------------------
+
+
+string evioDOMTree::toString() const throw(evioException*) {
+  ostringstream os;
+  toOstream(os,root,0);
+  os << endl << endl;
+  return(os.str());
+}
+
+
+//-----------------------------------------------------------------------------
+
+
+void evioDOMTree::toOstream(ostream &os, const evioDOMNode *pNode, int depth) const throw(evioException*) {
+
+  
+  if(pNode==NULL)return;
+
+
+  // dump this node into ostream
+  pNode->toString(os,depth);
+
+
+  // if node is container then dump contained banks
+  const evioDOMContainerNode *c = dynamic_cast<const evioDOMContainerNode*>(pNode);
+  if(c!=NULL) {
+    list<evioDOMNode*>::const_iterator iter;
+    for (iter=c->childList.begin(); iter!=c->childList.end(); iter++) {
+      toOstream(os,*iter,depth+1);
+    }
+  }
+
+}
+
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
--- evioUtil.hxx
+++ evioUtil.hxx
@@ -7,10 +7,17 @@
 #define _evioUtil_hxx
 
 using namespace std;
-#include <string>
-#include <sstream>
 #include <evio_util.h>
+#include <string>
+#include <list>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
 
+
+class evioDOMTree;
+class evioDOMNode;
 
 
 //--------------------------------------------------------------
@@ -70,6 +77,107 @@ private:
 
 //--------------------------------------------------------------
 //--------------------------------------------------------------
+
+
+class evioDOMHandler {
+
+public:
+  virtual void *nodeHandler(int length, int ftype, int tag, int type, int num, int depth, void *userArg) = 0;
+  virtual void leafHandler(int length, int ftype, int tag, int type, int num, int depth,
+                           const void *data, void *userArg) = 0;
+
+};
+
+
+//--------------------------------------------------------------
+//--------------------------------------------------------------
+
+
+class evioDOMParser {
+
+public:
+  evioDOMNode *parse(const unsigned long *buf) throw(evioException*);
+  
+};
+
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+
+class evioDOMTree {
+
+public:
+  evioDOMTree(const unsigned long *buf) throw(evioException*);
+  evioDOMTree(evioDOMNode *root) throw(evioException*);
+  virtual ~evioDOMTree(void);
+
+  string toString(void) const throw(evioException*);
+  //  void toEVIOBuffer(unsigned long *buf) const throw(evioException*);
+
+
+private:
+  evioDOMNode *root;
+  void toOstream(ostream &os, const evioDOMNode *node, int depth) const throw(evioException*);
+};
+
+
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+
+class evioDOMNode {
+
+public:
+  virtual ~evioDOMNode(void) {};
+
+  virtual string toString(void) const = 0;
+  virtual void toString(ostream &os, int depth) const = 0;
+
+public:
+  int containerType;
+  int tag;
+  int contentType;
+  int num;
+
+};
+
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+
+class evioDOMContainerNode:public evioDOMNode {
+
+public:
+  evioDOMContainerNode(int containerType, int tag, int contentType, int num) throw(evioException*);
+  virtual ~evioDOMContainerNode(void);
+
+  string toString(void) const;
+  void toString(ostream &os, int depth) const;
+
+  list<evioDOMNode*> childList;
+};
+
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+
+template <class T> class evioDOMLeafNode:public evioDOMNode {
+
+public:
+  evioDOMLeafNode(int containerType, int tag, int contentType, int num, T *p, int ndata) throw(evioException*);
+  string toString(void) const;
+  void toString(ostream &os, int depth) const;
+
+  vector<T> data;
+};
+
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 
 
 #endif
