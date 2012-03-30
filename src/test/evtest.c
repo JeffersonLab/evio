8394d9f93e3c84cdf1ceb090a21e285b69401701
Carl Timmer <timmer@jlab.org>
2012-03-30 17:52:14 +0000

    updates

--- src/test/evtest.c
+++ src/test/evtest.c
@@ -18,23 +18,54 @@
  
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "evio.h"
 
 int32_t *makeEvent();
 #define MIN(a,b) (a<b)? a : b
 
+char *dictionary =
+"<xmlDict>\n"
+"  <xmldumpDictEntry name=\"Tag1-Num1\"   tag=\"1\"   num=\"1\"/>\n"
+"  <xmldumpDictEntry name=\"Tag2-Num2\"   tag=\"2\"   num=\"2\"/>\n"
+"  <xmldumpDictEntry name=\"Tag3-Num3\"   tag=\"3\"   num=\"3\"/>\n"
+"  <xmldumpDictEntry name=\"Tag4-Num4\"   tag=\"4\"   num=\"4\"/>\n"
+"  <xmldumpDictEntry name=\"Tag5-Num5\"   tag=\"5\"   num=\"5\"/>\n"
+"  <xmldumpDictEntry name=\"Tag6-Num6\"   tag=\"6\"   num=\"6\"/>\n"
+"  <xmldumpDictEntry name=\"Tag7-Num7\"   tag=\"7\"   num=\"7\"/>\n"
+"  <xmldumpDictEntry name=\"Tag8-Num8\"   tag=\"8\"   num=\"8\"/>\n"
+"</xmlDict>\n";
+
+int32_t *makeEvent();
+int32_t *makeEvent2();
 
 int main()
 {
-    int handle, status, nevents, nlong, handle2;
-    int buffer[2048], i, maxEvBlk = 2;
-    int *ip, *pBuf;
+    int handle, status, nevents, nwords;
+    uint32_t *ip, *pBuf, buffer[2048], swappedBuffer[2048];
+    int i, maxEvBlk = 2;
+
 
     printf("\nEvent I/O tests...\n");
     status = evOpen("single.dat", "w", &handle);
     printf ("    Opened single.dat, status = %d\n", status);
 
-    pBuf = ip = makeEvent();
+    status = evWriteDictionary(handle, dictionary);
+
+    pBuf = ip = makeEvent2();
+
+    memset(buffer, 0, 2048);
+    memset(swappedBuffer, 0, 2048);
+    
+    evioswap(pBuf, 0, swappedBuffer);
+    evioswap(swappedBuffer, 1, buffer);
+
+    printf("  i      buffer      swapped 1x   swapped 2x\n");
+    printf("------------------------------------\n");
+    for (i=0; i < 39; i++) {
+        printf("  %d    %#10.8x      %#10.8x   %#10.8x\n", i, ip[i], swappedBuffer[i], buffer[i]);
+    }
+
     
     printf ("    Will write single.dat, status = %d\n",status);
     status = evWrite(handle, ip);
@@ -42,26 +73,37 @@ int main()
     status = evClose(handle);
     printf ("    Closed single.dat, status = %d\n\n", status);
 
-    status = evOpen("single.dat", "r", &handle2);
+    status = evOpen("single.dat", "r", &handle);
     nevents = 0;
     
-    while ((status = evRead(handle2, buffer, 2048)) == S_SUCCESS) {
+    while ((status = evRead(handle, buffer, 2048)) == S_SUCCESS) {
         nevents++;
-        printf("    Event #%d,  len = %d words\n", nevents, buffer[0]);
-        if (nevents <= 4) {
-            nlong = buffer[0] + 1;
-        }
         
-        for (ip=buffer; nlong > 0; nlong-=8) {
-            for (i = MIN(nlong,8); i > 0; i--) {
-                printf("      %#.8x\n", *ip++);
+        printf("    Event #%d,  len = %d data words\n", nevents, buffer[0] - 1);
+
+        ip = buffer;
+        nwords = buffer[0] + 1;
+        
+        printf("      Header words\n");
+        printf("        %#10.8x\n", *ip++);
+        printf("        %#10.8x\n\n", *ip++);
+        printf("      Data words\n");
+   
+        nwords -= 2;
+        
+        for (; nwords > 0; nwords-=4) {
+            for (i = MIN(nwords,4); i > 0; i--) {
+                printf("        %#10.8x", *ip++);
             }
+            printf("\n");
         }
-        
         printf("\n");
     }
+
+    printf ("    Last evRead status = %d\n", status);
+   
     
-    status = evClose(handle2);
+    status = evClose(handle);
     printf ("    Closed single.dat again, status = %d\n\n", status);
 
     ip = pBuf;
@@ -86,20 +128,28 @@ int main()
     
     while ((status = evRead(handle, buffer, 2048)) == S_SUCCESS) {
         nevents++;
-        printf("    Event #%d,  len = %d words\n", nevents, buffer[0]);
-        if (nevents <= 4) {
-            nlong = buffer[0] + 1;
-        }
         
-        for (ip=buffer; nlong > 0; nlong-=8) {
-            for (i = MIN(nlong,8); i > 0; i--) {
-                printf("      %#.8x\n", *ip++);
+        printf("    Event #%d,  len = %d data words\n", nevents, buffer[0] - 1);
+
+        ip = buffer;
+        nwords = buffer[0] + 1;
+        
+        printf("      Header words\n");
+        printf("        %#10.8x\n", *ip++);
+        printf("        %#10.8x\n\n", *ip++);
+        printf("      Data words\n");
+   
+        nwords -= 2;
+        
+        for (; nwords > 0; nwords-=4) {
+            for (i = MIN(nwords,4); i > 0; i--) {
+                printf("        %#10.8x", *ip++);
             }
+            printf("\n");
         }
-        
         printf("\n");
     }
-    
+   
     printf("\n    Last read, status = %x\n", status);
     if (status == EOF) {
         printf("    Last read, reached EOF!\n");
@@ -118,7 +168,7 @@ int32_t *makeEvent()
 
 
     bank = (int *) calloc(1, 11*sizeof(int32_t));
-    bank[0] = 10;                    /* event length = 9 */
+    bank[0] = 10;                    /* event length = 10 */
     bank[1] = 1 << 16 | 0x20 << 8;   /* tag = 1, bank 1 contains segments */
 
     segment = &(bank[2]);
@@ -139,70 +189,149 @@ int32_t *makeEvent()
     /* HI HO - 2 strings */
     segment[0] =    4 << 24 | 0x3 << 16 | 2; /* tag = 4, seg 3 has 2 strings, len = 2 */
     segment[1] = 0x48 << 24 | 0 << 16   | 0x49 << 8 | 0x48 ;   /* H \0 I H */
-    segment[2] =   4  << 24 | 4 << 16   | 0 << 8    | 0x4F ;   /* \4 \0 \0 O */
+    segment[2] =   4  << 24 | 4 << 16   | 0 << 8    | 0x4F ;   /* \4 \4 \0 O */
 
     return(bank);
 }
 
-
-int *makeEventOrig()
+int32_t *makeEvent2()
 {
-    int *bank;
-    int *segment, *longword;
-    short *word;
-    short *packet;
-    float data;
+    short *shortWord;
+    float  data;
+    double doubl;
+    uint64_t bigInt, *bigWord;
+    int32_t *bank, *segment, *tagseg, *intWord;
 
-    bank = (int *) malloc(80*sizeof(int));
-    bank[0] = 24;			/* event length = 18 */
-    bank[1] = 1<<16 | 0x20<<8;	/* bank 1 contains segments */
-    {
-        segment = &(bank[2]);
-        segment[0] = 1<<24 | 0x20<<16 | 6; /* segment 1 has 2 segments of len 3 */
-        {
-            segment = &(segment[1]);
-            segment[0] = 2<<24 |  1<<16 | 2; /* segment 2 has 2 longwords */
-            segment[1] = 0x11111111;
-            segment[2] = 0x22222222;
-            segment += 3;
-            segment[0] = 3<<24 |  4<<16 | 2; /* segment 3 has 2 longwords of shorts */
-            {
-                word = (short *) &(segment[1]);
-                word[0] = 0x0000;
-                word[1] = 0x1111;
-                word[2] = 0x2222;
-                word[3] = 0x3333;
-            }
-        }
-        segment = &(bank[2]) + 7;	/* point past segment 1 */
-        segment[0] = 4<<24 | 0x34<<16 | 3; /* seg 4 has I*2 packets */
-        {
-            packet = (short *) &(segment[1]);
-            packet[0] = 1<<8 | 2;	/* packet 1 */
-            packet[1] = 0x1111;
-            packet[2] = 0x2222;
-            packet += 3;
-            packet[0] = 2<<8 | 2;	/* packet 2 */
-            packet[1] = 0x1111;
-            packet[2] = 0x2222;
-        }
-        segment += 4;
-        segment[0] = 5<<24 | 0xF<<16 | 8; /* seg 5 contains repeating structures */
-        {
-            word = (short *) &(segment[1]);
-            word[0] = 2;
-            word[1] = 2<<8 | 2;	/* 2(a,b) */
-            word[2] = 0x8000 | 2<<4 | 1; /* 2I */
-            word[3] = 0x8000 | 1<<4 | 2; /* 1F */
-            longword = &(segment[3]);
-            data = 123.456;
-            longword[0] = 0x1111;
-            longword[1] = 0x2222;
-            longword[2] = *(int *)&data;
-            longword[3] = 0x11111111;
-            longword[4] = 0x22222222;
-            longword[5] = *(int *)&data;
-        }
-    }
+
+    bank = (int *) calloc(1, 39*sizeof(int32_t));
+
+    /* bank of banks */
+    bank[0] = 38;                        /* bank length (not including this int) */
+    bank[1] = 1 << 16 | 0x10 << 8 | 1;   /* tag = 1, bank contains banks, num = 1 */
+
+    
+    /********************/
+    /* bank of segments */
+    /********************/
+    bank[2] = 10;                        /* bank length (not including this int) */
+    bank[3] = 1 << 16 | 0x20 << 8 | 2;   /* tag = 1, bank contains segments, num = 2 */
+
+    /* segment of 32 bit ints */
+    segment = &(bank[4]);
+    segment[0] = 2 << 24 | 0xb << 16 | 2; /* tag = 2, seg 1 has 2 - 32 bit ints, len = 2 */
+    segment[1] = 0x1;
+    segment[2] = 0x2;
+        
+    segment += 3; /* bank[7] */
+        
+    /* segments of shorts */
+    segment[0] = 3 << 24 | 2 << 22 | 4 << 16 | 2; /* tag = 3, 2 bytes padding, seg 2 has 3 shorts, len = 2 */
+    shortWord = (short *) &(segment[1]);
+    shortWord[0] = 0x3;
+    shortWord[1] = 0x4;
+    shortWord[2] = 0x5;
+
+    segment += 3; /* bank[10] */
+
+    /* segment of string (HI HO - 2 strings - little endian) */
+    segment[0] =    4 << 24 | 0x3 << 16 | 2; /* tag = 4, seg 3 has 2 strings, len = 2 */
+    segment[1] = 0x48 << 24 | 0 << 16   | 0x49 << 8 | 0x48 ;   /* H \0 I H */
+    segment[2] =   4  << 24 | 4 << 16   | 0 << 8    | 0x4F ;   /* \4 \4 \0 O */
+
+    segment += 3; /* bank[13] */
+    
+
+    /***********************/
+    /* bank of tagsegments */
+    /***********************/
+    bank[13] = 25;                       /* bank length */
+    bank[14] = 6 << 16 | 0xC << 8 | 3;   /* tag = 6, bank contains tagsegments, num = 3 */
+
+    /* tagsegment of composite type */
+    tagseg = &(bank[15]);
+    tagseg[0] = 5 << 20 | 0xF << 16 | 17; /* tag = 5, seg has composite data, len = 17 */
+
+    /* first part of composite type (for format) = tagseg (tag & type ignored, len used) */
+    //    tagseg[1] = 5 << 20 | 0x3 << 16 | 3; /* tag = 5, seg has char data, len = 3 */
+    /* ASCII chars values in latest evio string (array) format, 2(2I,F) */
+    //    tagseg[2] = 0x49 << 24 | 0x32 << 16 | 0x28 << 8 | 0x32 ;   /* I 2 ( 2 */
+    //    tagseg[3] =   0  << 24 | 0x29 << 16 | 0x46 << 8 | 0x2C ;   /* \0 ) F , */
+    //    tagseg[4] =   4  << 24 |    4 << 16 |    4 << 8 |    4 ;   /* \4 \4 \4 \4 */
+
+    /* second part of composite type (for data) = bank (tag, num, type ignored, len used) */
+    /*
+    intWord = &(bank[20]);
+    intWord[0] = 7;
+    intWord[1] = 6 << 16 | 0xF << 8 | 1;
+    data = 123.456;
+    intWord[2] = 0x1111;
+    intWord[3] = 0x2222;
+    intWord[4] = *(int *)&data;
+    intWord[5] = 0x11111111;
+    intWord[6] = 0x22222222;
+    intWord[7] = *(int *)&data;
+    */
+
+    /* first part of composite type (for format) = tagseg (tag & type ignored, len used) */
+    //    tagseg[1] = 5 << 20 | 0x3 << 16 | 3; /* tag = 5, seg has char data, len = 3 */
+    /* ASCII chars values in latest evio string (array) format, I,N(I,F) */
+    //    tagseg[2] = 0x28 << 24 | 0x4E << 16 | 0x2C << 8 | 0x49;    /* ( N , I */
+    //    tagseg[3] = 0x29 << 24 | 0x46 << 16 | 0x2C << 8 | 0x49;    /* ) F , I */
+    //    tagseg[4] =   4  << 24 |    4 << 16 |    4 << 8 |    0 ;   /* \4 \4 \4 \0 */
+    
+    /* second part of composite type (for data) = bank (tag, num, type ignored, len used) */
+    /*
+    intWord = &(bank[20]);
+    intWord[0] = 7;
+    intWord[1] = 6 << 16 | 0xF << 8 | 1;
+    data = 123.456;
+    intWord[2] = 0x3333;
+    intWord[3] = 0x2;
+    intWord[4] = 0x1111;
+    intWord[5] = *(int *)&data;
+    intWord[6] = 0x2222;
+    intWord[7] = *(int *)&data;
+    */
+    
+    /* first part of composite type (for format) = tagseg (tag & type ignored, len used) */
+    tagseg[1] = 5 << 20 | 0x3 << 16 | 3; /* tag = 5, seg has char data, len = 3 */
+    /* ASCII chars values in latest evio string (array) format, N(N(I,2S)) with N=2 */
+    tagseg[2] = 0x28 << 24 | 0x4E << 16 | 0x28 << 8 | 0x4E;    /* ( N ( N */
+    tagseg[3] = 0x53 << 24 | 0x32 << 16 | 0x2C << 8 | 0x49;    /* S 2 , I */
+    tagseg[4] =   4  << 24 |    0 << 16 | 0x29 << 8 | 0x29 ;   /* \4 \0 ) ) */
+    
+    /* second part of composite type (for data) = bank (tag, num, type ignored, len used) */
+    
+    intWord = &(bank[20]);
+    intWord[0] = 12;
+    intWord[1] = 6 << 16 | 0xF << 8 | 1;
+    intWord[2] = 0x2; /* N */
+    intWord[3] = 0x2; /* N */
+    intWord[4] = 0x00001111;
+    intWord[5] = 0x11223344;
+    intWord[6] = 0x00002222;
+    intWord[7] = 0x55667788;
+    
+    intWord[8]  = 0x2; /* N */
+    intWord[9]  = 0x00003333;
+    intWord[10] = 0x00991188;
+    intWord[11] = 0x00004444;
+    intWord[12] = 0x22773366;
+    
+    
+    /* tagsegment of 64 bit ints, little endian */
+    bigInt = 0x0102030405060708L;
+    tagseg = &(bank[33]);
+    tagseg[0] = 7 << 20 | 0xa << 16 | 2; /* tag = 7, tagseg has 1 - 64 bit uint, len = 2 */
+    bigWord = (uint64_t *) &(tagseg[1]);
+    bigWord[0] = bigInt;
+      
+    /* tagsegment of double, little endian */
+    doubl = 123.;
+    tagseg = &(bank[36]);
+    tagseg[0] = 8 << 20 | 0x8 << 16 | 2; /* tag = 8, tagseg has 1 double, len = 2 */
+    bigWord = (uint64_t *) &(tagseg[1]);
+    bigWord[0] = *(uint64_t *)&doubl;
+
     return(bank);
 }
--- src/test/evtest2.c
+++ src/test/evtest2.c
@@ -15,47 +15,69 @@
 #include "evio.h"
 
 #define MAXBUFLEN  4096
+#define MIN(a,b) (a<b)? a : b
 
 
-
-int main (argc, argv)
-     int argc;
-     char **argv;
+int main (int argc, char **argv)
 {
-  int handle, nevents, status;
-  unsigned int buf[MAXBUFLEN];
-  int maxev =0;
+    int i, handle, nevents, status, bufLen, nWords, dLen, version;
+    uint32_t *buf, *ip;
+    char *dictionary = NULL;
+
+
+    if (argc != 2) {
+        printf("Incorrect number of arguments:\n");
+        printf("  usage: %s filename\n", argv[0]);
+        exit(-1);
+    }
+
+    if ( (status = evOpen(argv[1], "r", &handle)) < 0) {
+        printf("Unable to open file %s status = %d\n",argv[1],status);
+        exit(-1);
+    } else {
+        printf("Opened %s for reading, hit <enter> to see each individual event\n\n",argv[1]);
+    }
+
+    /* Get evio version # of file */
+    status = evIoctl(handle, "v", &version);
+    if (status == S_SUCCESS) {
+        printf("Evio file version = %d\n", version);
+    }
+
+    /* Get a dictionary if there is one */
+    status = evGetDictionary(handle, &dictionary, &dLen);
+    if (status == S_SUCCESS && dictionary != NULL) {
+        printf("Dictionary =\n%s\n\n", dictionary);
+        free(dictionary);
+    }
+
+    nevents=0;
+    while ((status = evReadAlloc(handle, &buf, &bufLen))==0) {
+        nevents++;
+        printf("    Event #%d,  len = %d words\n", nevents, buf[0]);
+        nWords = buf[0] + 1;
+
+        for (ip=buf; nWords > 0; nWords-=4) {
+            for (i = MIN(nWords,4); i > 0; i--) {
+                printf("      %#10.8x", *ip++);
+            }
+            printf("\n");
+        }
+       
+        printf("\n");
+
+        free(buf);
+        printf("Hit enter for next event\n");
+        getc(stdin);
+    }
+
+    if ( status == EOF ) {
+        printf("Hit end-of-file\n");
+    }
+    else {
+        printf("Error reading file, quit\n");
+    }
+    evClose(handle);
   
-
-  if(argc < 2) {
-    printf("Incorrect number of arguments:\n");
-    printf("  usage: evt filename [maxev]\n");
-    exit(-1);
-  }
-
-  if ( (status = evOpen(argv[1],"r",&handle)) < 0) {
-    printf("Unable to open file %s status = %d\n",argv[1],status);
-    exit(-1);
-  } else {
-    printf("Opened %s for reading\n",argv[1]);
-  }
-
-  if(argc==3) {
-    maxev = atoi(argv[2]);
-    printf("Read at most %d events\n",maxev);
-  } else {
-    maxev = 0;
-  }
-
-  nevents=0;
-  while ((status=evRead(handle,buf,MAXBUFLEN))==0) {
-    nevents++;
-    printf("  event %d len %u type %u\n",nevents,buf[0],(buf[1]&0xffff0000)>>16);
-    if((nevents >= maxev) && (maxev != 0)) break;
-  }
-
-  printf("last read status 0x%x\n",status);
-  evClose(handle);
-  
-  exit(0);
+    exit(0);
 }
