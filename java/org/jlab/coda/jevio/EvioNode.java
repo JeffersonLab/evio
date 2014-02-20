5dfc421f8d896c6aedcf4233e4e3dc0fb75f87df
Carl Timmer <timmer@jlab.org>
2014-02-20 15:29:06 +0000

    make cloneable, contains list of all contained nodes

--- java/org/jlab/coda/jevio/EvioNode.java
+++ java/org/jlab/coda/jevio/EvioNode.java
@@ -1,6 +1,16 @@
+/*
+ * Copyright (c) 2013, Jefferson Science Associates
+ *
+ * Thomas Jefferson National Accelerator Facility
+ * Data Acquisition Group
+ *
+ * 12000, Jefferson Ave, Newport News, VA 23606
+ * Phone : (757)-269-7100
+ *
+ */
+
 package org.jlab.coda.jevio;
 
-import java.nio.ByteBuffer;
 import java.util.LinkedList;
 
 /**
@@ -12,7 +22,7 @@ import java.util.LinkedList;
  * Date: 11/13/12
  * Time: 11:06 AM
  */
-public final class EvioNode {
+public final class EvioNode implements Cloneable {
 
     /** Header's length value (32-bit words). */
     int len;
@@ -41,14 +51,14 @@ public final class EvioNode {
     BlockNode blockNode;
 
     /** ByteBuffer that this node is associated with. */
-    ByteBuffer buffer;
+    BufferNode bufferNode;
 
     //-------------------------------
     // For event-level node
     //-------------------------------
 
     /**
-     * Place in file/buffer. First event = 0, second = 1, etc.
+     * Place of containing event in file/buffer. First event = 0, second = 1, etc.
      * Useful for converting node to EvioEvent object (de-serializing).
      */
     int place;
@@ -58,6 +68,9 @@ public final class EvioNode {
      * already placed into a list?
      */
     boolean scanned;
+
+    /** List of all nodes in the event including this (top level) object. */
+    LinkedList<EvioNode> allNodes;
 
     //-------------------------------
     // For sub event-level node
@@ -72,9 +85,77 @@ public final class EvioNode {
     /** List of child nodes ordered according to placement in buffer. */
     LinkedList<EvioNode> childNodes;
 
+    //----------------------------------
+    // Constructors (package accessible)
+    //----------------------------------
+
+    /** Constructor when fancy features not needed. */
+    EvioNode() {}
+
+
+    /**
+     * Constructor which creates an EvioNode associated with
+     * an event (top) from scratch in the CompactBuilder.
+     *
+     * @param tag      the tag for the event (or bank) header.
+   	 * @param dataType the data type contained in the event.
+   	 * @param num      the num for the event (or bank) header.
+     */
+    EvioNode(int tag, DataType dataType, int num) {
+        this.tag = tag;
+        this.num = num;
+        this.dataType = dataType.getValue();
+
+        // Put this node in list of all nodes (evio banks, segs, or tagsegs)
+        // contained in this event.
+        allNodes = new LinkedList<EvioNode>();
+        allNodes.addFirst(this);
+    }
+
+
+    /**
+     * Constructor which creates an EvioNode associated with
+     * an event (top) level evio container when parsing buffers
+     * for evio data.
+     *
+     * @param pos        position of event in buffer (number of bytes)
+     * @param place      containing event's place in buffer (starting at 1)
+     * @param bufferNode buffer containing this event
+     * @param blockNode  block containing this event
+     */
+    EvioNode(int pos, int place, BufferNode bufferNode, BlockNode blockNode) {
+        this.pos = pos;
+        this.place = place;
+        this.blockNode = blockNode;
+        this.bufferNode = bufferNode;
+        // This is an event by definition
+        this.isEvent = true;
+        // Event is a Bank by definition
+        this.type = DataType.BANK.getValue();
+
+        // Put this node in list of all nodes (evio banks, segs, or tagsegs)
+        // contained in this event.
+        allNodes = new LinkedList<EvioNode>();
+        allNodes.addFirst(this);
+    }
+
     //-------------------------------
     // Methods
     //-------------------------------
+
+    public Object clone() {
+        try {
+            EvioNode result = (EvioNode)super.clone();
+            // Doing things this way means we don't have to
+            // copy references to the node & buffer object.
+            // However, we do need a new list ...
+            result.childNodes = null;
+            return result;
+        }
+        catch (CloneNotSupportedException ex) {
+            return null;    // never invoked
+        }
+    }
 
     public String toString() {
         StringBuilder builder = new StringBuilder(100);
@@ -90,29 +171,63 @@ public final class EvioNode {
         return builder.toString();
     }
 
-    public void clear() {
-        len = 0;
-        tag = 0;
-        num = 0;
-        pad = 0;
-        pos = 0;
-        type = 0;
-        place = 0;
-        dataLen = 0;
-        dataPos = 0;
-        dataType = 0;
 
-        isEvent   = false;
-        scanned   = false;
-        buffer    = null;
-        blockNode = null;
-        eventNode = null;
+    public void clearLists() {
         if (childNodes != null) childNodes.clear();
+
+        // Should only be defined if this is an event (isEvent == true)
+        if (allNodes != null) {
+            allNodes.clear();
+            // Remember to add event's node into list
+            if (eventNode == null) {
+                allNodes.addFirst(this);
+            }
+            else {
+                allNodes.addFirst(eventNode);
+            }
+        }
+    }
+
+    //-------------------------------
+    // Setters
+    //-------------------------------
+
+    /**
+     * Add a child node to the end of the child list.
+     * @param childNode child node to add to the end of the child list.
+     */
+    synchronized public void addChild(EvioNode childNode) {
+        if (childNodes == null) {
+            childNodes = new LinkedList<EvioNode>();
+        }
+        childNodes.addLast(childNode);
+
+        if (eventNode != null && allNodes != null) allNodes.addLast(childNode);
+    }
+
+    /**
+     * Remove a node from this child list.
+     * @param childNode node to remove from child list.
+     */
+    synchronized public void removeChild(EvioNode childNode) {
+        if (childNodes != null) {
+            childNodes.remove(childNode);
+        }
+
+        if (eventNode != null && allNodes != null) allNodes.remove(childNode);
     }
 
     //-------------------------------
     // Getters
     //-------------------------------
+
+//    /**
+//     * Get the object representing the block header.
+//     * @return object representing the block header.
+//     */
+//    public BlockNode getBlockNode() {
+//        return blockNode;
+//    }
 
     /**
      * Get the length of this evio structure (not including length word itself)
@@ -122,6 +237,14 @@ public final class EvioNode {
      */
     public int getLength() {
         return len;
+    }
+
+    /**
+     * Get the length of this evio structure including entire header in bytes.
+     * @return length of this evio structure including entire header in bytes.
+     */
+    public int getTotalBytes() {
+        return 4*dataLen + dataPos - pos;
     }
 
     /**
@@ -164,8 +287,14 @@ public final class EvioNode {
      * returned value to get the object representation.
      * @return evio type of this evio structure, not what it contains
      */
-    public int getType() {
-        return type;
+    public int getType() {return type;}
+
+    /**
+     * Get the evio type of this evio structure as an object.
+     * @return evio type of this evio structure as an object.
+     */
+    public DataType getTypeObj() {
+        return DataType.getDataType(type);
     }
 
     /**
