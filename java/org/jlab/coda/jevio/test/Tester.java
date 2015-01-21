5c36807173ad9bbc58c654c09983abfc50075514
Carl Timmer <timmer@jlab.org>
2015-01-21 19:12:40 +0000

    clean up code

--- java/org/jlab/coda/jevio/test/Tester.java
+++ java/org/jlab/coda/jevio/test/Tester.java
@@ -499,25 +499,6 @@ System.out.println("Tag, int = " + tag + ", str = " + tagStr + ", bad entry = " 
                 e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             }
 
-            // option 9
-
-            try {
-                byte[] bArray = new byte[8*doubleData.length];
-                t1 = System.currentTimeMillis();
-                for (int j=0; j < LOOPS; j++) {
-                    byteBuffer.clear();
-                    ByteDataTransformer.toBytes2(doubleData, byteBuffer.order(), bArray, 0);
-                    byteBuffer.put(bArray);
-                }
-                t2 = System.currentTimeMillis();
-                deltaT = t2 - t1;
-                freq = (double) LOOPS / deltaT * 1000;
-                System.out.printf("loopRate D8: %2.3g Hz,  time = %2.3g sec\n", freq, (deltaT/1000.));
-                //printDoubleBuffer(byteBuffer);
-            }
-            catch (EvioException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            }
 
         }
 
