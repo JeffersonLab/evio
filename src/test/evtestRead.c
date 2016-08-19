6d10401333656af33aba0e5c80d09047f44943ac
Carl Timmer <timmer@jlab.org>
2016-08-19 19:58:05 +0000

    update

--- src/test/evtestRead.c
+++ src/test/evtestRead.c
@@ -14,8 +14,7 @@
  * Author:  Carl Timmer, Data Acquisition Group
  *
  */
- 
-#include <stdlib.h>
+
 #include <stdio.h>
 #include "evio.h"
 
@@ -25,14 +24,54 @@
 int main()
 {
     int handle, status, nevents, nwords, i;
+    uint32_t  buffer[50000], *ip;
+
+    status = evOpen("/home/timmer/evioTestFiles/evioV2format.ev", "r", &handle);
+    nevents = 0;
+
+    while ((status = evRead(handle, buffer, 50000)) == S_SUCCESS) {
+        nevents++;
+
+        printf("    Event #%d,  len = %d data bytes\n", nevents, 4*(buffer[0] - 1));
+
+        ip = buffer;
+        nwords = buffer[0] + 1;
+
+        printf("      Header words\n");
+        printf("        %#10.8x\n", *ip++);
+        printf("        %#10.8x\n\n", *ip++);
+        printf("      Data words\n");
+
+        nwords -= 2;
+
+
+//        printf("Hit Enter to continue:\n");
+//        getchar();
+    }
+
+    if (status == EOF) {
+        printf("    Last read, reached EOF!\n");
+    }
+    else {
+        printf ("    Last evRead status = %d, %s\n", status, evPerror(status));
+    }
+
+    status = evClose(handle);
+    printf ("    Closed /home/timmer/evioTestFiles/evioV2format.ev again, status = %d\n\n", status);
+}
+
+
+int main1()
+{
+    int handle, status, nevents, nwords, i;
     uint32_t  buffer[204800], *ip;
 
     status = evOpen("/tmp/fileTestSmallBigEndian.ev", "r", &handle);
     nevents = 0;
-    
+
     while ((status = evRead(handle, buffer, 204800)) == S_SUCCESS) {
         nevents++;
-        
+
         printf("    Event #%d,  len = %d data words\n", nevents, buffer[0] - 1);
 
         ip = buffer;
@@ -42,9 +81,9 @@ int main()
         printf("        %#10.8x\n", *ip++);
         printf("        %#10.8x\n\n", *ip++);
         printf("      Data words\n");
-   
+
         nwords -= 2;
-        
+
         for (; nwords > 0; nwords-=4) {
             for (i = MIN(nwords,4); i > 0; i--) {
                 printf("        %#10.8x", *ip++);
@@ -53,18 +92,18 @@ int main()
         }
         printf("\n");
 
-        
+
 //        printf("Hit Enter to continue:\n");
 //        getchar();
     }
-    
+
     if (status == EOF) {
         printf("    Last read, reached EOF!\n");
     }
     else {
         printf ("    Last evRead status = %d, %s\n", status, evPerror(status));
     }
-   
+
     status = evClose(handle);
     printf ("    Closed /daqfs/home/timmer/coda/output-evio4.ev again, status = %d\n\n", status);
 }
