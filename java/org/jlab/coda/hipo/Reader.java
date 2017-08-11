25b877d6c6bc0475f3f01d6ee454595258776d4b
gavalian <gavalian@meson.jlab.org>
2017-08-11 13:43:13 -0400

    added new file format implementation for jevio

---
+++ java/org/jlab/coda/hipo/Reader.java
@@ -0,0 +1,164 @@
+/*
+ *   Copyright (c) 2016.  Jefferson Lab (JLab). All rights reserved. Permission
+ *   to use, copy, modify, and distribute  this software and its documentation for
+ *   educational, research, and not-for-profit purposes, without fee and without a
+ *   signed licensing agreement.
+ */
+package org.jlab.coda.hipo;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Reader class that reads files stored in the HIPO
+ * format. 
+ * @author gavalian
+ * @date 08/10/2017
+ */
+public class Reader {
+    /**
+     * List of records in the file. The array is initialized
+     * at the open() method when the entire file is scanned
+     * to read out positions of each record in the file.
+     * 
+     */
+    private List<RecordEntry>  readerRecordEntries = 
+            new ArrayList<RecordEntry>();
+    /**
+     * Input binary file stream.
+     */
+    FileInputStream  inputStream = null;
+    
+    
+    /**
+     * Default constructor. Does nothing. If instance is created
+     * with default constructor the open() method has to be used 
+     * to open the input stream.
+     */
+    public Reader(){
+        
+    }
+    /**
+     * Constructor with filename. creates instance and opens 
+     * the input stream with given name
+     * @param filename input file name
+     */
+    public Reader(String filename){
+        open(filename);
+    }
+    /**
+     * Opens an input stream in binary mode. Scans for
+     * records in the file and stores record information
+     * in internal array. Each record can be read from the file.
+     * @param filename input file name
+     */
+    public final void open(String filename){
+        try {
+            inputStream = new FileInputStream(new File(filename));
+            this.scanFile();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Reader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /**
+     * Reads record from the file from position index.
+     * @param index record index
+     * @return decoded record from the file
+     */
+    public Record readRecord(int index){
+        Long   position = readerRecordEntries.get(index).getPosition();
+        Integer    size = readerRecordEntries.get(index).getLength();
+        byte[] buffer = new byte[size];
+        System.out.println(" READ RECORD SIZE = " + buffer.length);
+        try {
+            inputStream.getChannel().position(position);
+            inputStream.read(buffer);
+        } catch (IOException ex) {
+            Logger.getLogger(Reader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        Record rec = Record.initBinary(buffer);
+        return rec;
+    }
+    /**
+     * Returns the number of records recovered from the file.
+     * @return 
+     */
+    public int getRecordCount(){
+        return this.readerRecordEntries.size();
+    }
+    
+    private void scanFile(){
+        byte[]  fileHeader   = new byte[Writer.FILE_HEADER_LENGTH];
+        
+        try {
+            inputStream.read(fileHeader);
+            ByteBuffer buffer = ByteBuffer.wrap(fileHeader);
+            buffer.order(ByteOrder.LITTLE_ENDIAN);
+            Integer    headerLength = buffer.getInt(16);
+            
+            Long       recordPosition = (long) Writer.FILE_HEADER_LENGTH + headerLength;
+            Long      inputStreamSize = inputStream.getChannel().size();
+            byte[]  recordBuffer = new byte[48];
+            readerRecordEntries.clear();
+            System.out.println("--------->  RECORD POSITION " + recordPosition 
+                    + "  FILE SIZE = " + inputStreamSize);
+            while( (recordPosition + 48) < inputStreamSize ){
+                inputStream.getChannel().position(recordPosition);
+                inputStream.read(recordBuffer);
+                ByteBuffer header = ByteBuffer.wrap(recordBuffer);
+                header.order(ByteOrder.LITTLE_ENDIAN);
+                Integer recordLength = header.getInt(4);
+                Integer   eventCount = header.getInt(16);
+                RecordEntry entry = new RecordEntry(recordPosition,recordLength,eventCount);
+                readerRecordEntries.add(entry);
+                recordPosition += recordLength;
+            }
+            System.out.println(" recovered records = " + readerRecordEntries.size());
+        } catch (IOException ex) {
+            Logger.getLogger(Reader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void show(){
+        System.out.println("FILE: (info)");
+        for(RecordEntry entry : this.readerRecordEntries){
+            System.out.println(entry);
+        }
+    }
+    /**
+     * Internal class to keep track of the records in the file.
+     * Each entry keeps record position in the file, length of
+     * the record and number of entries contained.
+     */
+    public static class RecordEntry {
+        
+        Long     position = (long) 0;
+        Integer    length = 0;
+        Integer   entries = 0;
+        
+        public RecordEntry(long _pos, int _l, int _e){
+            position = _pos; length = _l; entries = _e;
+        }
+        public Long getPosition(){ return position;}
+        public Integer getLength(){ return length;}
+        public Integer getEntries(){return entries;}
+        
+        @Override
+        public String toString(){
+            StringBuilder str = new StringBuilder();
+            str.append(String.format("\t position : %9d, buffer count : %8d, length : %8d",
+                    position,entries,length));
+            return str.toString();
+        }
+    }
+}
---
+++ java/org/jlab/coda/hipo/Writer.java
@@ -0,0 +1,194 @@
+/*
+ *   Copyright (c) 2016.  Jefferson Lab (JLab). All rights reserved. Permission
+ *   to use, copy, modify, and distribute  this software and its documentation for
+ *   educational, research, and not-for-profit purposes, without fee and without a
+ *   signed licensing agreement.
+ */
+
+package org.jlab.coda.hipo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *  Writer class for CODA data files.
+ * @author gavalian
+ * @date 08/10/2017
+ */
+
+public class Writer {
+    /**
+     * Internal constants used in the FILE header
+     */
+    public final static Integer FILE_HEADER_LENGTH = 72;
+    public final static Integer FILE_UNIQUE_WORD   = 0x4849504F;
+    public final static Integer FILE_VERSION_WORD  = 0x56302E32;
+    /**
+     * output stream used for writing binary data to the file.
+     */
+    private FileOutputStream  outStream = null;    
+    private Record         outputRecord = null;
+    /**
+     * header byte buffer is stored when object is created.
+     * and all subsequent files will have same header byte buffer.
+     * the header is user defined and can be anything.
+     */
+    private byte[] writerHeaderBuffer = null;
+    
+    private Integer compressionType = 0;
+    
+    private Long    writerBytesWritten = 0L;
+    
+    /**
+     * default constructor only the internal record is initialized
+     * no file will be opened.
+     */
+    public Writer(){
+        outputRecord = new Record();
+    }
+    /**
+     * constructor with filename, the output file will be initialized.
+     * the header of the file will have zero length.
+     * @param filename output file name
+     */
+    public Writer(String filename){
+        outputRecord = new Record();
+        this.open(filename);
+    }
+    /**
+     * open a file with byte array that represents the header
+     * @param filename output file name
+     * @param header header array
+     */
+    public Writer(String filename, byte[] header){
+        outputRecord = new Record();
+        this.writerHeaderBuffer = header;
+        this.open(filename, header);
+    }
+    /**
+     * Open a file with given header and compression type
+     * @param filename output file name
+     * @param header header byte array
+     * @param compression compression type
+     */
+    public Writer(String filename, byte[] header, int compression){
+        outputRecord = new Record();
+        this.writerHeaderBuffer = header;
+        this.open(filename, header);
+        setCompressionType(compression);
+    }
+    /**
+     * Opens a file for writing with header array initialized from 
+     * string. The header string can be an XML dictionary.
+     * @param filename output file name
+     * @param header string header object 
+     * @param compression compression type
+     */
+    public Writer(String filename, String header, int compression){
+        outputRecord = new Record();
+        this.writerHeaderBuffer = header.getBytes();
+        open(filename, header.getBytes());
+        setCompressionType(compression);
+    }    
+    /**
+     * open a new file with no header.
+     * @param filename output file name
+     */
+    public final void open(String filename){
+        this.open(filename, new byte[]{});
+        
+    }
+    /**
+     * Open a file with given header byte array.
+     * @param filename disk file name
+     * @param header byte array representing the header
+     */
+    public final void open(String filename, byte[] header){
+        this.writerBytesWritten = (long) (Writer.FILE_HEADER_LENGTH + header.length);
+        try {
+            outStream = new FileOutputStream(new File(filename));
+            byte[] headerBuffer = new byte[Writer.FILE_HEADER_LENGTH+header.length];
+            ByteBuffer byteBuffer = ByteBuffer.wrap(headerBuffer);
+            byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+            
+            byteBuffer.putInt(  0, Writer.FILE_UNIQUE_WORD);
+            byteBuffer.putInt(  4, Writer.FILE_VERSION_WORD);
+            byteBuffer.putInt(  8, 0x2);
+            byteBuffer.putInt( 12, 0);
+            byteBuffer.putInt( 16, header.length);
+            
+            System.arraycopy(header, 0, headerBuffer, Writer.FILE_HEADER_LENGTH, header.length);
+            
+            outStream.write(headerBuffer);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Writer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Writer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    /**
+     * sets compression type for the file. The compression type
+     * is also set for internal record. When writing to the file
+     * record data will be compressed according to the type set.
+     * @param compression compression type
+     * @return this object
+     */
+    public final Writer setCompressionType(int compression){
+        this.outputRecord.setCompressionType(compression);
+        this.compressionType = outputRecord.getCompressionType();
+        return this;
+    }
+    /**
+     * Appends the record to the file.
+     * @param record record object
+     */
+    private void writeRecord(Record record){
+        ByteBuffer buffer = record.build();        
+        try {
+            byte[] bufferArray = buffer.array();
+            this.writerBytesWritten += bufferArray.length;
+            this.outStream.write(buffer.array());
+        } catch (IOException ex) {
+            Logger.getLogger(Writer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    /**
+     * add an byte buffer to the internal record. if the length of
+     * the buffer exceeds the maximum size of the record, the record
+     * will be written to the file (compressed if the flag is set), then
+     * it will be reset to receive new buffers.
+     * @param buffer array to add to the file.
+     */
+    public void addEvent(byte[] buffer){
+        boolean status = outputRecord.addEvent(buffer);
+        if(status==false){
+            //ByteBuffer outbytes = outputRecord.build();
+            writeRecord(outputRecord);
+            outputRecord.reset();
+            outputRecord.addEvent(buffer);
+        }
+    }
+    /**
+     * Close opened file. if the output record contains events.
+     * the events will be flushed out.
+     */
+    public void close(){
+        if(outputRecord.getEntries()>0){
+            System.out.println("[writer] ---> closing file. flashing " + 
+                    outputRecord.getEntries() + " events.");
+            this.writeRecord(outputRecord);
+        }
+        try {
+            this.outStream.close();
+            System.out.println("[writer] ---> bytes written " + this.writerBytesWritten);
+        } catch (IOException ex) {
+            Logger.getLogger(Writer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
