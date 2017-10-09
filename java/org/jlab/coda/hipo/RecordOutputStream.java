cf43f67b3aead8ee745aad3470425aad0245f9b8
Carl Timmer <timmer@jlab.org>
2017-10-09 13:31:34 -0400

    javadoc

--- java/org/jlab/coda/hipo/RecordOutputStream.java
+++ java/org/jlab/coda/hipo/RecordOutputStream.java
@@ -142,7 +142,7 @@ public class RecordOutputStream {
      * Constructor with arguments.
      * @param order byte order of built record byte arrays.
      * @param maxEventCount max number of events this record can hold.
-     *                      Value of O means use default (1M).
+     *                      Value <= O means use default (1M).
      * @param maxBufferSize max number of uncompressed data bytes this record can hold.
      *                      Value of < 8MB results in default of 8MB.
      * @param compressionType type of data compression to do (0=none, 1=lz4 fast, 2=lz4 best, 3=gzip).
