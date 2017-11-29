64f0f5fbc054358c9c91fbf20ed663d7c537958e
Stephen A. Wood <saw@jlab.org>
2017-11-29 12:01:54 -0500

    scons 3.0 compatibility fixes  scons 3.0 uses python3.  Python 3 doesn't like some things.   1.  print statements without parens around the stuff   2.  The old octal number format (Use 0o prefix instead of 0)   3.  Tab characters in the indentation.

--- coda.py
+++ coda.py
@@ -101,7 +101,7 @@ def is64BitMachine(env, platform, machine):
         ret = conf.CheckBits(ccflags)
         env = conf.Finish()
         if ret < 1:
-            print 'Cannot run test, assume 64 bit system'
+            print ('Cannot run test, assume 64 bit system')
             return True
         elif ret == 64:
             # Test shows 64 bit system'
@@ -153,8 +153,8 @@ def getInstallationDirs(osname, prefix, incdir, libdir, bindir):
         # prefix not defined try CODA env var
         if codaHomeEnv == "":
             if (incdir == None) or (libdir == None) or (bindir == None):
-                print "\nNeed to define CODA, or use the --prefix option,"
-                print "or all the --incdir, --libdir, and --bindir options.\n"
+                print ("\nNeed to define CODA, or use the --prefix option,")
+                print ("or all the --incdir, --libdir, and --bindir options.\n")
                 raise SystemExit
         else:
             prefix = codaHomeEnv
@@ -203,7 +203,7 @@ def makeIncludeDirs(includeDir, archIncludeDir, archDir, archIncLocalLink):
     # Make sure it's a directory (if we didn't create it)
     elif not os.path.isdir(includeDir):
         print
-        print "Error:", includeDir, "is NOT a directory"
+        print ("Error:", includeDir, "is NOT a directory")
         print
         raise SystemExit
 
@@ -221,7 +221,7 @@ def makeIncludeDirs(includeDir, archIncludeDir, archDir, archIncLocalLink):
             return
     elif not os.path.isdir(archDir):
         print
-        print "Error:", archDir, "is NOT a directory"
+        print ("Error:", archDir, "is NOT a directory")
         print
         raise SystemExit
 
@@ -232,17 +232,17 @@ def makeIncludeDirs(includeDir, archIncludeDir, archDir, archIncLocalLink):
     if not os.path.exists(archIncludeDir):
         # Create symbolic link: symlink(source, linkname)
         try:
-    	    if (archIncLocalLink == None) or (archIncLocalLink == ''):
-	    	symlink(includeDir, archIncludeDir)
+            if (archIncLocalLink == None) or (archIncLocalLink == ''):
+                symlink(includeDir, archIncludeDir)
             else:
-	    	symlink(archIncLocalLink, archIncludeDir)
+                symlink(archIncLocalLink, archIncludeDir)
         except OSError:
             # Failed to create symbolic link, so
             # just make it a regular directory
             os.makedirs(archIncludeDir)
     elif not os.path.isdir(archIncludeDir):
         print
-        print "Error:", archIncludeDir, "is NOT a directory"
+        print ("Error:", archIncludeDir, "is NOT a directory")
         print
         raise SystemExit
 
@@ -266,18 +266,18 @@ def configureJNI(env):
             java_base = '/System/Library/Frameworks/JavaVM.framework'
         else:
             # Search for the java compiler
-            print "JAVA_HOME environment variable not set. Searching for javac to find jni.h ..."
+            print ("JAVA_HOME environment variable not set. Searching for javac to find jni.h ...")
             if not env.get('JAVAC'):
-                print "The Java compiler must be installed and in the current path, exiting"
+                print ("The Java compiler must be installed and in the current path, exiting")
                 return 0
             jcdir = os.path.dirname(env.WhereIs('javac'))
             if not jcdir:
-                print "   not found, exiting"
+                print ("   not found, exiting")
                 return 0
             # assuming the compiler found is in some directory like
             # /usr/jdkX.X/bin/javac, java's home directory is /usr/jdkX.X
             java_base = os.path.join(jcdir, "..")
-            print "  found, dir = " + java_base        
+            print ("  found, dir = " + java_base)
         
     if sys.platform == 'darwin':
         # Apple does not use Sun's naming convention
