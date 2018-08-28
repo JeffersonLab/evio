0b4e59bae15ac671b9f9082d7dad71f57112339a
Carl Timmer <timmer@jlab.org>
2018-08-28 16:11:17 -0400

    add static eventParse method

--- java/org/jlab/coda/jevio/EventParser.java
+++ java/org/jlab/coda/jevio/EventParser.java
@@ -9,12 +9,154 @@ import javax.swing.tree.DefaultTreeModel;
  * Creates an object that controls the parsing of events.
  * This object, like the EvioReader object, has a method for parsing an event. An EvioReader
  * object will ultimately call this method--i.e., the concrete implementation of event
- * parsing is in this class.
+ * parsing is in this class.<p>
+ * There is also a static method to do the parsing of an event, but without notifications.
  * 
  * @author heddle
  * @author timmer
  */
 public class EventParser {
+
+
+    /**
+	 * Method for parsing the event which will drill down and uncover all structures.
+	 * @param evioEvent the event to parse.
+	 * @throws EvioException if arg is null.
+	 */
+	static public void eventParse(EvioEvent evioEvent) throws EvioException {
+
+		if (evioEvent == null) {
+			throw new EvioException("Null event in parseEvent.");
+		}
+
+		// The event itself is a structure (EvioEvent extends EvioBank) so just
+		// parse it as such. The recursive drill down will take care of the rest.
+		parseStruct(evioEvent, evioEvent);
+	}
+
+	/**
+	 * Parse a structure. If it is a structure of structures, such as a bank of banks or a segment of tag segments,
+	 * parse recursively.
+	 *
+	 * @param evioEvent the parent event being processed.
+	 * @param structure the structure being processed.
+	 * @throws EvioException if data not in evio format
+	 */
+	static private void parseStruct(EvioEvent evioEvent, BaseStructure structure) throws EvioException {
+
+		// update the tree
+		BaseStructure parent = structure.getParent();
+
+		if (parent == null) { // start of event
+			evioEvent.treeModel = new DefaultTreeModel(evioEvent);
+		}
+		else {
+			evioEvent.insert(structure, parent);
+		}
+
+		// If it is not a structure of structures, we are done. That will leave the raw bytes in
+		// the "leaf" structures (e.g., a bank of ints) --which will be interpreted by the various get data methods.
+		DataType dataType = structure.getHeader().getDataType();
+		if (!dataType.isStructure()) {
+			return;
+		}
+
+		// Does the present structure contain structures? (as opposed to a leaf, which contains primitives). If
+		// it is a leaf we are done. That will leave the raw bytes in the "leaf" structures (e.g., a bank of ints)
+        // --which will be interpreted by the various "get data" methods.
+        byte bytes[] = structure.getRawBytes();
+        ByteOrder byteOrder = structure.getByteOrder();
+
+        if (bytes == null) {
+            throw new EvioException("Null data in parseStructure (Bank).");
+        }
+
+        int length = bytes.length;
+        int offset = 0;
+
+        switch (dataType) {
+            case BANK:
+            case ALSOBANK:
+
+                // extract all the banks from this bank.
+                while (offset < length) {
+                    BankHeader header = createBankHeader(bytes, offset, byteOrder);
+
+                    // offset still points to beginning of new header. Have to get data for new child bank.
+                    int newByteLen = 4 * (header.getLength() - 1); // -1 to account for extra header word
+                    byte newBytes[] = new byte[newByteLen];
+                    System.arraycopy(bytes, offset + 8, newBytes, 0, newByteLen);
+
+                    // we can now create a new bank
+                    EvioBank childBank = new EvioBank(header);
+                    childBank.setParent(structure);
+
+                    childBank.setRawBytes(newBytes);
+                    childBank.setByteOrder(byteOrder);
+                    parseStruct(evioEvent, childBank);
+
+                    // position offset to start of next header
+                    offset += 4 * (header.getLength() + 1); // plus 1 for length word
+                }
+                break; // structure contains banks
+
+            case SEGMENT:
+            case ALSOSEGMENT:
+
+                // extract all the segments from this bank.
+                while (offset < length) {
+                    SegmentHeader header = createSegmentHeader(bytes, offset, byteOrder);
+
+                    // offset still points to beginning of new header. Have to get data for new child segment.
+                    int newByteLen = 4 * header.getLength();
+                    byte newBytes[] = new byte[newByteLen];
+                    System.arraycopy(bytes, offset + 4, newBytes, 0, newByteLen);
+
+                    // we can now create a new segment
+                    EvioSegment childSegment = new EvioSegment(header);
+                    childSegment.setParent(structure);
+
+                    childSegment.setRawBytes(newBytes);
+                    childSegment.setByteOrder(byteOrder);
+                    parseStruct(evioEvent, childSegment);
+
+                    // position offset to start of next header
+                    offset += 4 * (header.getLength() + 1); // plus 1 for length word
+                }
+
+                break; // structure contains segments
+
+            case TAGSEGMENT:
+//			case ALSOTAGSEGMENT:
+
+                // extract all the tag segments from this bank.
+                while (offset < length) {
+                    TagSegmentHeader header = createTagSegmentHeader(bytes, offset, byteOrder);
+
+                    // offset still points to beginning of new header. Have to get data for new child tag segment.
+                    int newByteLen = 4 * header.getLength();
+                    byte newBytes[] = new byte[newByteLen];
+                    System.arraycopy(bytes, offset + 4, newBytes, 0, newByteLen);
+
+                    // we can now create a new tag segment
+                    EvioTagSegment childTagSegment = new EvioTagSegment(header);
+                    childTagSegment.setParent(structure);
+
+                    childTagSegment.setRawBytes(newBytes);
+                    childTagSegment.setByteOrder(byteOrder);
+                    parseStruct(evioEvent, childTagSegment);
+
+                    // position offset to start of next header
+                    offset += 4 * (header.getLength() + 1); // plus 1 for length word
+                }
+
+                break;
+
+            default:
+        }
+	}
+
+    // Now the non-static parser
 
 	/**
 	 * Listener list for structures (banks, segments, tagsegments) encountered while processing an event.
@@ -33,6 +175,7 @@ public class EventParser {
 	 */
 	private IEvioFilter evioFilter;
 
+
 	/**
 	 * This is the workhorse method for parsing the event. It will drill down and uncover all structures
      * (banks, segments, and tagsegments) and notify any interested listeners in a SAX-Like manner. <br>
@@ -40,7 +183,7 @@ public class EventParser {
 	 * may be accessed through the event's tree model, i.e., via <code>event.getTreeModel()</code>.
 	 * 
 	 * @param evioEvent the event to parse.
-	 * @throws EvioException if arg is null.
+	 * @throws EvioException if arg is null or data not in evio format.
 	 */
 	public synchronized void parseEvent(EvioEvent evioEvent) throws EvioException {
 
@@ -73,7 +216,7 @@ public class EventParser {
 	 * 
 	 * @param evioEvent the parent event being processed.
 	 * @param structure the structure being processed.
-	 * @throws EvioException if data not in evio format
+	 * @throws EvioException if data not in evio format.
 	 */
 	private void parseStructure(EvioEvent evioEvent, BaseStructure structure) throws EvioException {
 
