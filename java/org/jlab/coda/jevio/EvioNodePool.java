d30ca072cc8aacf531a8ba8a818898f4da20bb87
Carl Timmer <timmer@jlab.org>
2018-12-10 15:02:05 -0500

    give each pool a unique id for debugging, add getSize, getUsed and getId

--- java/org/jlab/coda/jevio/EvioNodePool.java
+++ java/org/jlab/coda/jevio/EvioNodePool.java
@@ -11,39 +11,70 @@
 
 package org.jlab.coda.jevio;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Array-based pool of EvioNode objects. This class is currently used for
- * parsing Evio data in event builders.
+ * parsing Evio data in event builders. Not thread safe.
  */
 public class EvioNodePool implements EvioNodeSource {
+
     /** Pool of EvioNode objects used for parsing Evio data in EBs. */
     private EvioNode[] nodePool;
 
-    /** Current number of the pool objects in use. */
+    /** Index into nodePool array of the next pool object to use. */
     private int poolIndex;
 
     /** Total size of the pool. */
-    private int poolLimit;
+    private int size;
 
-    
+    /** Id of this pool. For debugging. */
+    private int id;
+
+    /** For assigning pool id numbers. */
+    static private AtomicInteger idCounter = new AtomicInteger();
+
+
+
     /**
      * Constructor.
      * @param initialSize number of EvioNode objects in pool initially.
      */
     public EvioNodePool(int initialSize) {
-        poolLimit = initialSize;
-        nodePool = new EvioNode[poolLimit];
+        id = idCounter.getAndIncrement();
+        size = initialSize;
+        nodePool = new EvioNode[size];
         // Create pool objects
-        for (int i=0; i < poolLimit; i++) {
+        for (int i = 0; i < size; i++) {
             nodePool[i] = new EvioNode();
+            nodePool[i].poolId = id;
         }
     }
 
 
+    /**
+     * Get the id number of this pool.
+     * Used for debugging.
+     * @return id number of this pool.
+     */
+    public int getId() {return id;}
+
+    /**
+     * Get the number of nodes taken from pool.
+     * @return number of nodes taken from pool.
+     */
+    public int getUsed() {return poolIndex;}
+
+    /**
+     * Get the number of nodes in the pool.
+     * @return number of nodes in the pool.
+     */
+    public int getSize() {return size;}
+
     /** {@inheritDoc} */
     public EvioNode getNode() {
         int currentIndex = poolIndex;
-        if (poolIndex++ >= poolLimit) {
+        if (poolIndex++ >= size) {
             // It's better to increase the pool so these objects
             // don't get reallocate with each new event being parsed.
             increasePool();
@@ -64,19 +95,19 @@ public class EvioNodePool implements EvioNodeSource {
     /** Increase the size of the pool by 20% or at least 1. */
     private void increasePool() {
         // Grow it by 20% at a shot
-        int newPoolLimit = poolLimit + (poolLimit + 4)/5;
+        int newPoolSize = size + (size + 4)/5;
 
-        EvioNode[] newPool = new EvioNode[newPoolLimit];
+        EvioNode[] newPool = new EvioNode[newPoolSize];
 
         // Copy over references of existing pool objects
-        System.arraycopy(nodePool, 0, newPool, 0, poolLimit);
+        System.arraycopy(nodePool, 0, newPool, 0, size);
 
         // Create new pool objects
-        for (int i=poolLimit; i < newPoolLimit; i++) {
+        for (int i = size; i < newPoolSize; i++) {
             newPool[i] = new EvioNode();
         }
 
-        nodePool  = newPool;
-        poolLimit = newPoolLimit;
+        nodePool = newPool;
+        size = newPoolSize;
     }
 }
