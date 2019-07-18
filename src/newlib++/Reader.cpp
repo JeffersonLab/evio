90477fee8aa9500c81096cb03852acf45be2dc52
Unknown <unknown@example.com>
2019-07-18 16:45:00 -0400

    add files

---
+++ src/newlib++/Reader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by timmer on 7/18/19.
+//
+
+#include "Reader.h"
---
+++ src/newlib++/Reader.h
@@ -0,0 +1,14 @@
+//
+// Created by timmer on 7/18/19.
+//
+
+#ifndef EVIO_6_0_READER_H
+#define EVIO_6_0_READER_H
+
+
+class Reader {
+
+};
+
+
+#endif //EVIO_6_0_READER_H
