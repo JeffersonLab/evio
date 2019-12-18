6735201adb8ae30f270648003cc45b020af3f768
Unknown <unknown@example.com>
2019-12-18 15:37:54 -0500

    update

--- java/org/jlab/coda/jevio/test/CompactReaderTest.java
+++ java/org/jlab/coda/jevio/test/CompactReaderTest.java
@@ -8,6 +8,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.*;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -882,80 +883,55 @@ public class CompactReaderTest {
 
         int evCount;
 
-
         try {
-            EvioCompactReader reader = new EvioCompactReader("/daqfs/home/timmer/rafopar044.evio");
+            String filename = "/home/timmer/evioDataFiles/clas_006586.evio.00001";
 
             // Get each event in the buffer
-            evCount = reader.getEventCount();
-            System.out.println("got Count = " + evCount);
+            int mmapCount = 0;
+            int compositeBanksCount = 0;
 
-//            Scanner keyboard = new Scanner(System.in);
-//            keyboard.next().charAt(0);
-//
+            // Looking for memory leak as this file is read, over and over again
+            while (true) {
 
+                EvioCompactReader reader = new EvioCompactReader(filename);
+                evCount = reader.getEventCount();
+                //System.out.println("got Count = " + evCount);
 
+                for (int i = 0; i < evCount; i++) {
+                    EvioNode node = reader.getScannedEvent(i+1);
+                    ArrayList<EvioNode> kids = node.getChildNodes();
+                    //System.out.println("event " + i + ": has kid count = " + kids.size());
 
-//when I do the following on that file:
-//
-//         ByteBuffer     compBuffer = node.getByteData(true);
-//          CompositeData  compData = new CompositeData(compBuffer.array(),event.getByteOrder());
-//
-//         For the node tag=57617 and num=anything, I get the exception thrown:
-//
-//         illegal character (value 0)
-//
-//         it comes from here (LINE 1574) in CompositeData :
-//
-//         throw new EvioException("illegal character (value " + (byte)ch + ")");
+                    for (EvioNode kid : node.getChildNodes()) {
+                        // For look for bank of banks
+                        if (kid.getDataTypeObj().isBank()) {
 
-
-                int num = 0;
-                List<EvioNode> list = reader.searchEvent(2, 57601, num);
-        //    System.out.println("list = " + list);
-                if (!list.isEmpty()) {
-
-                    System.out.println("List not empty for event " + (2) + " tag = 57617, num = " + num);
-                    for (EvioNode node : list) {
-                        ByteBuffer compBuffer = node.getByteData(true);
-                        try {
-                            CompositeData compData = new CompositeData(compBuffer.array(), reader.getByteOrder());
-                            System.out.println("COMPOSITE DATA:\n" + compData);
-                        }
-                        catch (EvioException e) {
-                            // print out the bad bytes
-                            Utilities.printBuffer(compBuffer, 0, 100, "Bad composite bank");
-                            throw e;
+                            ArrayList<EvioNode> grkids = kid.getChildNodes();
+                            //System.out.println("    bank kid count = " + grkids.size());
+                            for (EvioNode grandkid : grkids) {
+                                //System.out.println("    grkid type = " + grandkid.getDataTypeObj());
+                                if (grandkid.getDataTypeObj() == DataType.COMPOSITE) {
+                                    //System.out.println("    got composite");
+                                    // Got a bank of composite data
+                                    ByteBuffer buf = grandkid.getByteData(true);
+                                    CompositeData data = new CompositeData(buf.array(), reader.getByteOrder());
+                                    compositeBanksCount++;
+                                }
+                            }
                         }
                     }
                 }
 
-//            for (int i=0; i < evCount; i++) {
-//                int num = 0;
-//                List<EvioNode> list = reader.searchEvent(i+1, 57617, num);
-//                if (!list.isEmpty()) {
-//
-//                    System.out.println("List not empty for event " + (i+1) + " tag = 57617, num = " + num);
-//                    for (EvioNode node : list) {
-//                        ByteBuffer compBuffer = node.getByteData(true);
-//                        try {
-//                            CompositeData compData = new CompositeData(compBuffer.array(), reader.getByteOrder());
-//                        }
-//                        catch (EvioException e) {
-//                            // print out the bad bytes
-//                            Utilities.printBuffer(compBuffer, 0, 100, "Bad composite bank");
-//                            throw e;
-//                        }
-//                    }
-//                }
-//            }
+                if (++mmapCount % 5 == 0) {
+                    System.out.println("mmapCount = " + mmapCount +
+                            ", compositeBankCOunt = " + compositeBanksCount);
+                }
 
-            reader.close();
 
-        }
-        catch (EvioException e) {
-            e.printStackTrace();
-            System.exit(-1);
+
+                reader.close();
+                //System.gc();
+            }
         }
         catch (Exception e) {
             e.printStackTrace();
