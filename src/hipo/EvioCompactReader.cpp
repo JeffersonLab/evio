2e1c1596066b4247a7ff3ddb8a4d21bb27cfe1b1
Carl Timmer <timmer@jlab.org>
2020-06-18 16:21:30 -0400

    save progress

--- src/hipo/EvioCompactReader.cpp
+++ src/hipo/EvioCompactReader.cpp
@@ -3,3 +3,283 @@
 //
 
 #include "EvioCompactReader.h"
+
+
+
+namespace evio {
+
+
+    /**
+     * Constructor for reading an event file.
+     *
+     * @param path the full path to the file that contains events.
+     *             For writing event files, use an <code>EventWriter</code> object.
+     * @see org.jlab.coda.jevio.EventWriter
+     * @throws java.io.IOException   if read failure
+     * @throws org.jlab.coda.jevio.EvioException if file arg is null
+     */
+    EvioCompactReader::EvioCompactReader(string const & path, bool synced) {
+
+        if (path.empty()) {
+            throw EvioException("path is empty");
+        }
+
+        /** Object for reading file. */
+        ifstream inStreamRandom;
+        inStreamRandom.open(path, std::ios::binary);
+
+        initialPosition = 0;
+
+        // Create buffer of size 32 bytes
+        size_t bytesToRead = 32;
+        ByteBuffer headerBuffer(bytesToRead);
+        auto headerBytes = headerBuffer.array();
+
+        // Read first 32 bytes of file header
+        inStreamRandom.read(reinterpret_cast<char *>(headerBytes), bytesToRead);
+
+        // Parse file header to find the file's endianness & evio version #
+        evioVersion = Util::findEvioVersion(headerBuffer, initialPosition);
+        byteOrder = headerBuffer.order();
+
+        // This object is no longer needed
+        inStreamRandom.close();
+
+        if (evioVersion < 5) {
+            reader = std::make_shared<EvioCompactReaderV4>(path, synced);
+        }
+        else if (evioVersion == 6) {
+            reader = std::make_shared<EvioCompactReaderV6>(path, synced);
+        }
+        else {
+            throw  EvioException("unsupported evio version (" + std::to_string(evioVersion) + ")");
+        }
+    }
+
+
+    /**
+     * Constructor for reading a buffer with option of removing synchronization
+     * for much greater speed.
+     * @param bb the buffer that contains events.
+     * @param synced     if true, methods are synchronized for thread safety, else false.
+     * @see EventWriter
+     * @throws BufferUnderflowException if not enough buffer data;
+     * @throws EvioException if buffer arg is null;
+     *                       failure to parse first block header;
+     *                       unsupported evio version.
+     */
+    EvioCompactReader::EvioCompactReader(std::shared_ptr<ByteBuffer> & bb, bool synced) {
+
+        byteBuffer = bb;
+        initialPosition = byteBuffer->position();
+
+        // Parse file header to find the buffer's endianness & evio version #
+        evioVersion = Util::findEvioVersion(*(byteBuffer.get()), initialPosition);
+        byteOrder = byteBuffer->order();
+
+        if (evioVersion < 4)  {
+            throw EvioException("unsupported evio version (" + std::to_string(evioVersion) + "), only 4+");
+        }
+
+        if (evioVersion == 4) {
+            reader = std::make_shared<EvioCompactReaderV4>(byteBuffer, synced);
+        }
+        else if (evioVersion == 6) {
+            reader = std::make_shared<EvioCompactReaderV6>(byteBuffer, synced);
+        }
+        else {
+            throw EvioException("unsupported evio version (" + std::to_string(evioVersion) + ")");
+        }
+    }
+
+
+    /**
+     * Constructor for reading a buffer with option of removing synchronization
+     * for much greater speed.
+     * @param byteBuffer the buffer that contains events.
+     * @param synced     if true, methods are synchronized for thread safety, else false.
+     * @see EventWriter
+     * @throws BufferUnderflowException if not enough buffer data;
+     * @throws EvioException if buffer arg is null;
+     *                       failure to parse first block header;
+     *                       unsupported evio version.
+     */
+    EvioCompactReader::EvioCompactReader(std::shared_ptr<ByteBuffer> & bb, EvioNodeSource & pool, bool synced) {
+
+        byteBuffer = bb;
+        initialPosition = byteBuffer->position();
+
+        // Parse file header to find the buffer's endianness & evio version #
+        evioVersion = Util::findEvioVersion(*(byteBuffer.get()), initialPosition);
+        byteOrder = byteBuffer->order();
+
+        if (evioVersion < 4)  {
+            throw EvioException("unsupported evio version (" + std::to_string(evioVersion) + "), only 4+");
+        }
+
+        if (evioVersion == 4) {
+            reader = std::make_shared<EvioCompactReaderV4>(byteBuffer, pool, synced);
+        }
+        else if (evioVersion == 6) {
+            reader = std::make_shared<EvioCompactReaderV6>(byteBuffer, pool, synced);
+        }
+        else {
+            throw EvioException("unsupported evio version (" + std::to_string(evioVersion) + ")");
+        }
+    }
+
+    /** {@inheritDoc} */
+    bool EvioCompactReader::isFile() {return reader->isFile();}
+
+    /** {@inheritDoc} */
+    bool EvioCompactReader::isCompressed() {return reader->isCompressed();}
+
+    /** {@inheritDoc} */
+    void EvioCompactReader::setBuffer(std::shared_ptr<ByteBuffer> & buf) {reader->setBuffer(buf);}
+
+    /** {@inheritDoc} */
+    void EvioCompactReader::setBuffer(std::shared_ptr<ByteBuffer> & buf, EvioNodeSource & pool) {
+        reader->setBuffer(buf, pool);
+    }
+
+    /** {@inheritDoc} */
+    std::shared_ptr<ByteBuffer> EvioCompactReader::setCompressedBuffer(std::shared_ptr<ByteBuffer> & buf, EvioNodeSource & pool) {
+        return reader->setCompressedBuffer(buf, pool);
+    }
+
+    /** {@inheritDoc} */
+    /*synchronized*/bool EvioCompactReader::isClosed() {return reader->isClosed();}
+
+    /** {@inheritDoc} */
+    ByteOrder EvioCompactReader::getByteOrder() {return reader->getByteOrder();}
+
+    /** {@inheritDoc} */
+    uint32_t EvioCompactReader::getEvioVersion() {return evioVersion;}
+
+    /** {@inheritDoc} */
+    string EvioCompactReader::getPath() {return reader->getPath();}
+
+    /** {@inheritDoc} */
+    ByteOrder EvioCompactReader::getFileByteOrder() {return reader->getFileByteOrder();}
+
+    /** {@inheritDoc} */
+    /*synchronized*/string EvioCompactReader::getDictionaryXML() {
+        return reader->getDictionaryXML();
+    }
+
+    /** {@inheritDoc} */
+    /*synchronized*/EvioXMLDictionary EvioCompactReader::getDictionary() {
+        return reader->getDictionary();
+    }
+
+    /** {@inheritDoc} */
+    bool EvioCompactReader::hasDictionary() {return reader->hasDictionary();}
+
+    //    /** {@inheritDoc} */
+    //    MappedByteBuffer EvioCompactReader::getMappedByteBuffer() {return reader->getMappedByteBuffer();}
+
+    /** {@inheritDoc} */
+    std::shared_ptr<ByteBuffer> EvioCompactReader::getByteBuffer() {return reader->getByteBuffer();}
+
+    /** {@inheritDoc} */
+    size_t EvioCompactReader::fileSize() {return reader->fileSize();}
+
+
+    /** {@inheritDoc} */
+    EvioNode EvioCompactReader::getEvent(size_t eventNumber) {
+        return reader->getEvent(eventNumber);
+    }
+
+    /** {@inheritDoc} */
+    EvioNode EvioCompactReader::getScannedEvent(size_t eventNumber) {
+        return reader->getScannedEvent(eventNumber);
+    }
+
+    /** {@inheritDoc} */
+    EvioNode EvioCompactReader::getScannedEvent(size_t evNumber, EvioNodeSource & nodeSource) {
+        return reader->getScannedEvent(evNumber, nodeSource);
+    }
+
+    /** {@inheritDoc} */
+    std::shared_ptr<IBlockHeader> EvioCompactReader::getFirstBlockHeader() {
+        return reader->getFirstBlockHeader();
+    }
+
+    /** {@inheritDoc} */
+    /*synchronized*/void EvioCompactReader::searchEvent(size_t evNumber, uint16_t tag, uint8_t num,
+                                                        std::vector<EvioNode> & vec) {
+        ///*synchronized*/List<EvioNode> searchEvent(size_t evNumber, uint16_t tag, uint8_t num) {
+        return reader->searchEvent(evNumber, tag, num, vec);
+    }
+
+    /** {@inheritDoc} */
+    /*synchronized*/void EvioCompactReader::searchEvent(size_t eventNumber, string const & dictName,
+                                     EvioXMLDictionary & dictionary, std::vector<EvioNode> & vec) {
+        ///*synchronized*/List<EvioNode> searchEvent(int eventNumber, string dictName,
+        //                                           EvioXMLDictionary & dictionary) {
+
+        return reader->searchEvent(eventNumber, dictName, dictionary, vec);
+    }
+
+    /** {@inheritDoc} */
+    /*synchronized*/ByteBuffer EvioCompactReader::removeEvent(size_t eventNumber) {
+        return reader->removeEvent(eventNumber);
+    }
+
+    /** {@inheritDoc} */
+    /*synchronized*/ByteBuffer EvioCompactReader::removeStructure(EvioNode & removeNode) {
+        return reader->removeStructure(removeNode);
+    }
+
+    /** {@inheritDoc} */
+    /*synchronized*/ByteBuffer EvioCompactReader::addStructure(size_t eventNumber, ByteBuffer & addBuffer) {
+        return reader->addStructure(eventNumber, addBuffer);
+    }
+
+    /** {@inheritDoc} */
+    ByteBuffer EvioCompactReader::getData(EvioNode & node) {
+        return reader->getData(node);
+    }
+
+    /** {@inheritDoc} */
+    /*synchronized*/ByteBuffer EvioCompactReader::getData(EvioNode & node, bool copy) {
+        return reader->getData(node, copy);
+    }
+
+    /** {@inheritDoc} */
+    ByteBuffer EvioCompactReader::getEventBuffer(size_t eventNumber) {
+        return reader->getEventBuffer(eventNumber);
+    }
+
+    /** {@inheritDoc} */
+    /*synchronized*/ByteBuffer EvioCompactReader::getEventBuffer(size_t eventNumber, bool copy) {
+        return reader->getEventBuffer(eventNumber, copy);
+    }
+
+    /** {@inheritDoc} */
+    ByteBuffer EvioCompactReader::getStructureBuffer(EvioNode & node) {
+        return reader->getStructureBuffer(node);
+    }
+
+    /** {@inheritDoc} */
+    /*synchronized*/ByteBuffer EvioCompactReader::getStructureBuffer(EvioNode & node, bool copy) {
+        return reader->getStructureBuffer(node, copy);
+    }
+
+
+    /** {@inheritDoc} */
+    /*synchronized*/void EvioCompactReader::close() {reader->close();}
+
+    /** {@inheritDoc} */
+    uint32_t EvioCompactReader::getEventCount() {return reader->getEventCount();}
+
+    /** {@inheritDoc} */
+    uint32_t EvioCompactReader::getBlockCount() {return reader->getBlockCount();}
+
+    /** {@inheritDoc} */
+    void EvioCompactReader::toFile(string fileName) {
+        reader->toFile(fileName);
+    }
+
+
+}
--- src/hipo/EvioCompactReader.h
+++ src/hipo/EvioCompactReader.h
@@ -6,9 +6,128 @@
 #define EVIO_6_0_EVIOCOMPACTREADER_H
 
 
-class EvioCompactReader {
+#include <fstream>
+#include <stdexcept>
 
-};
 
+#include "IEvioCompactReader.h"
+#include "EvioReader.h"
+#include "Util.h"
+
+
+namespace evio {
+
+
+
+    /**
+     * This class is used to read an evio version 4 formatted file or buffer
+     * and extract specific evio containers (bank, seg, or tagseg)
+     * with actual data in them given a tag/num pair. It is theoretically thread-safe
+     * if synced is true. It is designed to be fast and does <b>NOT</b> do a full deserialization
+     * on each event examined.<p>
+     *
+     * @author timmer
+     */
+    class EvioCompactReader : public IEvioCompactReader {
+
+    private:
+
+        /** Evio version number (1-4, 6). Obtain this by reading first header. */
+        uint32_t evioVersion = 4;
+
+        /**
+         * Endianness of the data being read, either
+         * {@link java.nio.ByteOrder#BIG_ENDIAN} or
+         * {@link java.nio.ByteOrder#LITTLE_ENDIAN}.
+         */
+        ByteOrder byteOrder {ByteOrder::ENDIAN_LITTLE};
+
+        /** The buffer being read. */
+        std::shared_ptr<ByteBuffer> byteBuffer;
+
+        /** Initial position of buffer (0 if file). */
+        size_t initialPosition = 0UL;
+
+        //------------------------
+        // Object to delegate to
+        //------------------------
+        std::shared_ptr<IEvioCompactReader> reader;
+
+
+    public:
+
+        EvioCompactReader(string const & path, bool synced = false);
+        EvioCompactReader(std::shared_ptr<ByteBuffer> & byteBuffer, bool synced = false) ;
+        EvioCompactReader(std::shared_ptr<ByteBuffer> & byteBuffer, EvioNodeSource & pool, bool synced) ;
+
+    public:
+
+        bool isFile() override;
+        bool isCompressed() override;
+
+        void setBuffer(std::shared_ptr<ByteBuffer> & buf) override;
+        void setBuffer(std::shared_ptr<ByteBuffer> & buf, EvioNodeSource & pool) override;
+        std::shared_ptr<ByteBuffer> setCompressedBuffer(std::shared_ptr<ByteBuffer> & buf, EvioNodeSource & pool) override;
+
+        /*synchronized*/bool isClosed() override;
+
+        ByteOrder getByteOrder() override;
+        uint32_t getEvioVersion() override;
+        string getPath() override;
+        ByteOrder getFileByteOrder() override;
+
+        /*synchronized*/string getDictionaryXML() override ;
+        /*synchronized*/EvioXMLDictionary getDictionary() override ;
+
+        bool hasDictionary() override;
+
+//    /** {@inheritDoc} */
+//    MappedByteBuffer getMappedByteBuffer() {return reader->getMappedByteBuffer();}
+
+        std::shared_ptr<ByteBuffer> getByteBuffer() override;
+
+        size_t fileSize() override;
+
+        EvioNode getEvent(size_t eventNumber) override;
+        EvioNode getScannedEvent(size_t eventNumber) override;
+        EvioNode getScannedEvent(size_t eventNumber, EvioNodeSource & nodeSource) override ;
+
+        std::shared_ptr<IBlockHeader> getFirstBlockHeader() override;
+
+        /*synchronized*/void searchEvent(size_t evNumber, uint16_t tag, uint8_t num, std::vector<EvioNode> & vec) override ;
+
+        /*synchronized*/void searchEvent(size_t eventNumber, string const & dictName,
+                                 EvioXMLDictionary & dictionary, std::vector<EvioNode> & vec) override ;
+
+        /*synchronized*/ByteBuffer removeEvent(size_t eventNumber) override;
+        /*synchronized*/ByteBuffer removeStructure(EvioNode & removeNode) override;
+        /*synchronized*/ByteBuffer addStructure(size_t eventNumber, ByteBuffer & addBuffer) override;
+
+        ByteBuffer getData(EvioNode & node) override;
+        /*synchronized*/ByteBuffer getData(EvioNode & node, bool copy) override;
+
+        ByteBuffer getEventBuffer(size_t eventNumber) override;
+        /*synchronized*/ByteBuffer getEventBuffer(size_t eventNumber, bool copy) override;
+
+        ByteBuffer getStructureBuffer(EvioNode & node) override;
+        /*synchronized*/ByteBuffer getStructureBuffer(EvioNode & node, bool copy) override;
+
+        /*synchronized*/void close() override;
+
+        uint32_t getEventCount() override;
+        uint32_t getBlockCount() override;
+
+        void toFile(string fileName) override;
+
+//        /** {@inheritDoc} */
+//        /*synchronized*/void toFile(File file) {
+//                reader->toFile(file);
+//        }
+
+    };
+
+
+
+}
 
 #endif //EVIO_6_0_EVIOCOMPACTREADER_H
