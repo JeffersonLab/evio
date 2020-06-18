5e6372bd40b64acfedc3f3dcc4d324e6e6ec3c30
Carl Timmer <timmer@jlab.org>
2020-06-18 12:37:35 -0400

    translate from java

--- src/hipo/EvioReaderV6.cpp
+++ src/hipo/EvioReaderV6.cpp
@@ -3,3 +3,320 @@
 //
 
 #include "EvioReaderV6.h"
+
+namespace evio {
+
+
+    //------------------------
+
+
+    /**
+     * Constructor for reading an event file.
+     *
+     * @param path the full path to the file that contains events.
+     * @param checkSeq if <code>true</code> check the record number sequence
+     *                       and throw an exception if it is not sequential starting
+     *                       with 1
+     *
+     * @see EventWriter
+     * @throws IOException   if read failure
+     * @throws EvioException if file arg is null;
+     *                       if file is too small to have valid evio format data
+     *                       if first record number != 1 when checkRecNumSeq arg is true
+     */
+    EvioReaderV6::EvioReaderV6(string const & path, bool checkSeq, bool synced) {
+        if (path.empty()) {
+            throw EvioException("path is empty");
+        }
+        synchronized = synced;
+        reader = std::make_shared<Reader>(path);
+        parser = std::make_shared<EventParser>();
+     }
+
+
+    /**
+     * Constructor for reading a buffer.
+     *
+     * @param byteBuffer the buffer that contains events.
+     * @param checkRecNumSeq if <code>true</code> check the record number sequence
+     *                       and throw an exception if it is not sequential starting
+     *                       with 1
+     * @see EventWriter
+     * @throws EvioException if buffer arg is null;
+     *                       if first record number != 1 when checkRecNumSeq arg is true
+     */
+    EvioReaderV6::EvioReaderV6(std::shared_ptr<ByteBuffer> & byteBuffer, bool checkRecNumSeq, bool synced) {
+        synchronized = synced;
+        reader = std::make_shared<Reader>(byteBuffer);
+        parser = std::make_shared<EventParser>();
+    }
+
+    /**
+     * This method can be used to avoid creating additional EvioReader
+     * objects by reusing this one with another buffer. The method
+     * {@link #close()} is called before anything else.
+     *
+     * @param buf ByteBuffer to be read
+     * @throws IOException   if read failure
+     * @throws EvioException if buf is null;
+     *                       if first record number != 1 when checkRecNumSeq arg is true
+     */
+    void EvioReaderV6::setBuffer(std::shared_ptr<ByteBuffer> & buf) {
+        if (synchronized) {
+            const std::lock_guard<std::mutex> lock(mtx);
+        }
+        reader->setBuffer(buf);
+    }
+
+    /** {@inheritDoc} */
+    bool  /* sync */ EvioReaderV6::isClosed() {return reader->isClosed();}
+
+    /** {@inheritDoc} */
+    bool EvioReaderV6::checkBlockNumberSequence() {return reader->getCheckRecordNumberSequence();}
+
+    /** {@inheritDoc} */
+    ByteOrder & EvioReaderV6::getByteOrder() {return reader->getByteOrder();}
+
+    /** {@inheritDoc} */
+    uint32_t EvioReaderV6::getEvioVersion() {return reader->getVersion();}
+
+    /** {@inheritDoc} */
+    string EvioReaderV6::getPath() {return reader->getFileName();}
+
+    /**
+     * Get the file/buffer parser.
+     * @return file/buffer parser.
+     */
+    std::shared_ptr<EventParser> & EvioReaderV6::getParser() {return parser;}
+
+    /**
+     * Set the file/buffer parser.
+     * @param p file/buffer parser.
+     */
+    void EvioReaderV6::setParser(std::shared_ptr<EventParser> & p) {
+        if (p != nullptr) {
+            this->parser = p;
+        }
+    }
+
+    /** {@inheritDoc} */
+    string EvioReaderV6::getDictionaryXML() {return reader->getDictionary();}
+
+    /** {@inheritDoc} */
+    bool EvioReaderV6::hasDictionaryXML() {return reader->hasDictionary();}
+
+    /**
+     * Get the number of events remaining in the file.
+     * Useful only if doing a sequential read.
+     *
+     * @return number of events remaining in the file
+     */
+    size_t EvioReaderV6::getNumEventsRemaining() {return  reader->getNumEventsRemaining();}
+
+    /** {@inheritDoc} */
+    std::shared_ptr<ByteBuffer> EvioReaderV6::getByteBuffer() {return reader->getBuffer();}
+
+    /** {@inheritDoc} */
+    size_t EvioReaderV6::fileSize() {return reader->getFileSize();}
+
+
+    /** {@inheritDoc} */
+    std::shared_ptr<IBlockHeader> EvioReaderV6::getFirstBlockHeader() {return reader->getFirstRecordHeader();}
+
+
+    /**
+     * Get the event in the file/buffer at a given index (starting at 1).
+     * As useful as this sounds, most applications will probably call
+     * {@link #parseNextEvent()} or {@link #parseEvent(int)} instead,
+     * since it combines combines getting an event with parsing it.<p>
+     *
+     * @param  index the event number in a 1,2,..N counting sense, from beginning of file/buffer.
+     * @return the event in the file/buffer at the given index or null if none
+     * @throws EvioException if failed file access;
+     *                       if failed read due to bad file/buffer format;
+     *                       if object closed
+     */
+    std::shared_ptr<EvioEvent> EvioReaderV6::getEvent(size_t index) {
+        if (synchronized) {
+            const std::lock_guard<std::mutex> lock(mtx);
+        }
+
+        std::vector<uint8_t> vec;
+        uint32_t len = getEventArray(index, vec);
+        return EvioReader::getEvent(vec.data(), len, reader->getByteOrder());
+    }
+
+
+    /** {@inheritDoc} */
+    std::shared_ptr<EvioEvent> EvioReaderV6::parseEvent(size_t index) {
+        if (synchronized) {
+            const std::lock_guard<std::mutex> lock(mtx);
+        }
+
+        auto event = getEvent(index);
+        if (event != nullptr) parseEvent(event);
+        return event;
+    }
+
+
+    /** {@inheritDoc} */
+    std::shared_ptr<EvioEvent> EvioReaderV6::nextEvent() {
+        if (synchronized) {
+            const std::lock_guard<std::mutex> lock(mtx);
+        }
+
+        if (closed) {
+            throw EvioException("object closed");
+        }
+
+        uint32_t len;
+        shared_ptr<uint8_t> bytes = reader->getNextEvent(&len);
+        return EvioReader::getEvent(bytes.get(), len, reader->getByteOrder());
+    }
+
+
+    /** {@inheritDoc} */
+    std::shared_ptr<EvioEvent> EvioReaderV6::parseNextEvent() {
+        if (synchronized) {
+            const std::lock_guard<std::mutex> lock(mtx);
+        }
+
+        auto event = nextEvent();
+        if (event != nullptr) {
+            parseEvent(event);
+        }
+        return event;
+    }
+
+
+    /** {@inheritDoc} */
+    void EvioReaderV6::parseEvent(std::shared_ptr<EvioEvent> evioEvent) {
+            // This method is synchronized too
+            parser->parseEvent(evioEvent);
+    }
+
+
+    /** {@inheritDoc} */
+    uint32_t EvioReaderV6::getEventArray(size_t evNumber, std::vector<uint8_t> & vec) {
+        if (closed) {
+            throw EvioException("object closed");
+        }
+
+        uint32_t len;
+        shared_ptr<uint8_t> evBytes = reader->getEvent(evNumber - 1, &len);
+        if (evBytes == nullptr) {
+            throw EvioException("eventNumber (" + std::to_string(evNumber) + ") is out of bounds");
+        }
+
+        vec.clear();
+        vec.reserve(len);
+
+        // copy data into provided vector ...
+        std::memcpy(vec.data(), evBytes.get(), len);
+        return len;
+    }
+
+
+    /** {@inheritDoc} */
+    uint32_t EvioReaderV6::getEventBuffer(size_t evNumber, ByteBuffer & buf) {
+        uint32_t len;
+        shared_ptr<uint8_t> evBytes = reader->getEvent(evNumber - 1, &len);
+        if (evBytes == nullptr) {
+            throw EvioException("eventNumber (" + std::to_string(evNumber) + ") is out of bounds");
+        }
+
+        buf.clear();
+        buf.expand(len);
+
+        // copy data over
+        std::memcpy(buf.array() + buf.arrayOffset(), evBytes.get(), len);
+        return len;
+    }
+
+
+    /** This method is not relevant in evio 6 and does nothing. */
+    void EvioReaderV6::rewind() {}
+
+
+    /** This method is not relevant in evio 6, does nothing, and returns 0. */
+    size_t EvioReaderV6::position() {return 0UL;}
+
+
+    /**
+     * This is closes the file, but for buffers it only sets the position to 0.
+     * @throws IOException if error accessing file
+     */
+    void EvioReaderV6::close() {
+        if (synchronized) {
+            const std::lock_guard<std::mutex> lock(mtx);
+        }
+
+        if (closed) {
+            return;
+        }
+
+        reader->close();
+        closed = true;
+    }
+
+
+    /** {@inheritDoc} */
+    std::shared_ptr<IBlockHeader> EvioReaderV6::getCurrentBlockHeader() {
+        return reader->getCurrentRecordStream().getHeader();
+    }
+
+
+    /**
+     * In this version, this method is a wrapper on {@link #parseEvent(int)}.
+     *
+     * @deprecated use {@link #parseEvent(int)}.
+     * @param evNumber the event number from the start of the file starting at 1.
+     * @return the specified event in file or null if there's an error or nothing at that event #.
+     * @throws EvioException if object closed
+     */
+    std::shared_ptr<EvioEvent> EvioReaderV6::gotoEventNumber(size_t evNumber) {
+        if (synchronized) {
+            const std::lock_guard<std::mutex> lock(mtx);
+        }
+
+        if (closed) {
+            throw EvioException("object closed");
+        }
+
+        try {
+            return parseEvent(evNumber);
+        }
+        catch (EvioException & e) {
+            return nullptr;
+        }
+    }
+
+
+
+    /** {@inheritDoc} */
+    size_t EvioReaderV6::getEventCount() {
+        if (synchronized) {
+            const std::lock_guard<std::mutex> lock(mtx);
+        }
+
+        if (closed) {
+            throw EvioException("object closed");
+        }
+        return reader->getEventCount();
+    }
+
+
+    /** {@inheritDoc} */
+    size_t EvioReaderV6::getBlockCount() {
+        if (synchronized) {
+            const std::lock_guard<std::mutex> lock(mtx);
+        }
+
+        if (closed) {
+            throw EvioException("object closed");
+        }
+        return reader->getRecordCount();
+    }
+
+
+}
--- src/hipo/EvioReaderV6.h
+++ src/hipo/EvioReaderV6.h
@@ -5,10 +5,131 @@
 #ifndef EVIO_6_0_EVIOREADERV6_H
 #define EVIO_6_0_EVIOREADERV6_H
 
+#include <fstream>
+#include <vector>
+#include <memory>
+#include <mutex>
 
-class EvioReaderV6 {
+#include "ByteOrder.h"
+#include "ByteBuffer.h"
+#include "IEvioReader.h"
+#include "EvioReader.h"
+#include "IBlockHeader.h"
+#include "BlockHeaderV2.h"
+#include "BlockHeaderV4.h"
+#include "EventParser.h"
+#include "Reader.h"
 
-};
+
+namespace evio {
+
+    /**
+     * This class is used to read an evio version 6 format file or buffer.
+     * It is called by an <code>EvioReader</code> object. This class is mostly
+     * a wrapper to the new hipo library.<p>
+     *
+     * The streaming effect of parsing an event is that the parser will read the event and hand off structures,
+     * such as banks, to any IEvioListeners. For those familiar with XML, the event is processed SAX-like.
+     * It is up to the listener to decide what to do with the structures.
+     * <p>
+     *
+     * As an alternative to stream processing, after an event is parsed, the user can use the events treeModel
+     * for access to the structures. For those familiar with XML, the event is processed DOM-like.
+     * <p>
+     *
+     * @since version 6
+     * @author timmer
+     * @date 6/16/2020
+     */
+    class EvioReaderV6 : public IEvioReader {
+
+    private:
+
+        /** The reader object which does all the work. */
+        std::shared_ptr<Reader> reader;
+
+        /** Is this object currently closed? */
+        bool closed = false;
+
+        /** Parser object for file/buffer. */
+        std::shared_ptr<EventParser> parser;
+
+        /** Is this library made completely thread-safe? */
+        bool synchronized = false;
+
+        /** Mutex used for making thread safe. */
+        std::mutex mtx;
+
+        //------------------------
+        // File specific members
+        //------------------------
+
+//        /** Absolute path of the underlying file. */
+//        string path;
+//
+//        /** File input stream. */
+//        ifstream file;
+//
+//        /** File size in bytes. */
+//        size_t fileBytes = 0;
+//
+//        /** Do we need to swap data from file? */
+//        bool swap = false;
+//
+//        /**
+//         * Read this file sequentially and not using a memory mapped buffer.
+//         * If the file being read > 2.1 GBytes, then this is always true.
+//         */
+//        bool sequentialRead = false;
+
+
+        //------------------------
+
+    public:
+
+        explicit EvioReaderV6(string const & path, bool checkRecNumSeq = false, bool synced = false);
+        explicit EvioReaderV6(std::shared_ptr<ByteBuffer> & byteBuffer, bool checkRecNumSeq = false, bool synced = false);
+
+
+        /*synchronized */ void setBuffer(std::shared_ptr<ByteBuffer> & buf) override ;
+        /*synchronized*/ bool isClosed() override ;
+        bool checkBlockNumberSequence() override ;
+        ByteOrder & getByteOrder() override ;
+        uint32_t getEvioVersion() override ;
+        string getPath()override ;
+
+        std::shared_ptr<EventParser> & getParser() override ;
+        void setParser(std::shared_ptr<EventParser> & parser) override ;
+
+        string getDictionaryXML() override ;
+        bool hasDictionaryXML() override ;
+
+        size_t getNumEventsRemaining() override ;
+        std::shared_ptr<ByteBuffer> getByteBuffer() override ;
+        size_t fileSize() override ;
+        std::shared_ptr<IBlockHeader> getFirstBlockHeader() override ;
+
+        /*synchronized*/ std::shared_ptr<EvioEvent> getEvent(size_t index) override ;
+        /*synchronized*/ std::shared_ptr<EvioEvent> parseEvent(size_t index) override ;
+        /*synchronized*/ std::shared_ptr<EvioEvent> nextEvent() override ;
+        /*synchronized*/ std::shared_ptr<EvioEvent> parseNextEvent() override ;
+        void parseEvent(std::shared_ptr<EvioEvent> evioEvent) override ;
+
+        uint32_t getEventArray(size_t evNumber, std::vector<uint8_t> & vec) override;
+        uint32_t getEventBuffer(size_t evNumber, ByteBuffer & buf) override;
+
+        void rewind() override ;
+        /*synchronized */ size_t position() override ;
+        /*synchronized*/ void close() override ;
+
+        std::shared_ptr<IBlockHeader> getCurrentBlockHeader() override ;
+        /*synchronized*/ std::shared_ptr<EvioEvent> gotoEventNumber(size_t evNumber) override ;
+
+        /*synchronized*/ size_t getEventCount() override ;
+        /*synchronized*/ size_t getBlockCount() override ;
+    };
+
+}
 
 
 #endif //EVIO_6_0_EVIOREADERV6_H
