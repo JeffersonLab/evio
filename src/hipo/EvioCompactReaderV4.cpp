d1167ad97c158f423fdb6bd26d7d3a16d7779aa2
Carl Timmer <timmer@jlab.org>
2020-07-01 17:29:22 -0400

    add to repository

---
+++ src/hipo/EvioCompactReaderV4.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Carl Timmer on 7/1/20.
+//
+
+#include "EvioCompactReaderV4.h"
---
+++ src/hipo/EvioCompactReaderV4.h
@@ -0,0 +1,14 @@
+//
+// Created by Carl Timmer on 7/1/20.
+//
+
+#ifndef EVIO_6_0_EVIOCOMPACTREADERV4_H
+#define EVIO_6_0_EVIOCOMPACTREADERV4_H
+
+
+class EvioCompactReaderV4 {
+
+};
+
+
+#endif //EVIO_6_0_EVIOCOMPACTREADERV4_H
