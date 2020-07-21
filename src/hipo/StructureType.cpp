d918fa0a229befdec02429ad0d01e2a7a6aa615d
Carl Timmer <timmer@jlab.org>
2020-07-21 14:39:51 -0400

    updated header comment

--- src/hipo/StructureType.cpp
+++ src/hipo/StructureType.cpp
@@ -17,65 +17,66 @@
 namespace evio {
 
 
-bool StructureType::operator==(const StructureType &rhs) const {
-    return value == rhs.value;
-}
+    bool StructureType::operator==(const StructureType &rhs) const {
+        return value == rhs.value;
+    }
 
-bool StructureType::operator!=(const StructureType &rhs) const {
-    return value != rhs.value;
-}
+    bool StructureType::operator!=(const StructureType &rhs) const {
+        return value != rhs.value;
+    }
 
 
-// "Enum" value DEFINITIONS
+    // "Enum" value DEFINITIONS
 
-/** Unknown data type. */
-const StructureType StructureType::STRUCT_UNKNOWN32 = StructureType("UNKNOWN32", 0);
-/** Tag segment. */
-const StructureType StructureType::STRUCT_TAGSEGMENT = StructureType("TAGSEGMENT", 0xc);
-/** Bank. */
-const StructureType StructureType::STRUCT_BANK = StructureType("BANK", 0x10, 0xe);
-/** Segment. */
-const StructureType StructureType::STRUCT_SEGMENT = StructureType("SEGMENT", 0x20, 0xd);
+    /** Unknown data type. */
+    const StructureType StructureType::STRUCT_UNKNOWN32 = StructureType("UNKNOWN32", 0);
+    /** Tag segment. */
+    const StructureType StructureType::STRUCT_TAGSEGMENT = StructureType("TAGSEGMENT", 0xc);
+    /** Bank. */
+    const StructureType StructureType::STRUCT_BANK = StructureType("BANK", 0x10, 0xe);
+    /** Segment. */
+    const StructureType StructureType::STRUCT_SEGMENT = StructureType("SEGMENT", 0x20, 0xd);
 
-// Initialize static members
-std::string StructureType::names[33] =
-        {"UNKNOWN32", "UNKNOWN32",
+    // Initialize static members
+    std::string StructureType::names[33] = {
+            "UNKNOWN32", "UNKNOWN32",
 
-         // Unused, just fill with unknown
-         "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32",
-         "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32",
+            // Unused, just fill with unknown
+            "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32",
+            "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32",
 
-         "TAGSEGMENT",
-         "SEGMENT",
-         "BANK",
-         "UNKNOWN32",
-         "BANK",
+            "TAGSEGMENT",
+            "SEGMENT",
+            "BANK",
+            "UNKNOWN32",
+            "BANK",
 
-         "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32",
-         "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32",
-         "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32",
+            "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32",
+            "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32",
+            "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32", "UNKNOWN32",
 
-         "SEGMENT"
-        };
+            "SEGMENT"
+    };
 
-StructureType StructureType::intToType[33] =
-        {StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32,
-         StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32,
-         StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32,
-         StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32,
+    StructureType StructureType::intToType[33] = {
+            StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32,
+            StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32,
+            StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32,
+            StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32,
 
-         StructureType::STRUCT_TAGSEGMENT,
-         StructureType::STRUCT_SEGMENT,
-         StructureType::STRUCT_BANK,
-         StructureType::STRUCT_UNKNOWN32,
-         StructureType::STRUCT_BANK,
+            StructureType::STRUCT_TAGSEGMENT,
+            StructureType::STRUCT_SEGMENT,
+            StructureType::STRUCT_BANK,
+            StructureType::STRUCT_UNKNOWN32,
+            StructureType::STRUCT_BANK,
 
-         StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32,
-         StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32,
-         StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32,
-         StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32,
-         StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32,
+            StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32,
+            StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32,
+            StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32,
+            StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32,
+            StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32, StructureType::STRUCT_UNKNOWN32,
 
-         StructureType::STRUCT_SEGMENT};
+            StructureType::STRUCT_SEGMENT
+    };
 
 }
