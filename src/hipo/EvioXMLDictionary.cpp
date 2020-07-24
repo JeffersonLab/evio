77255c3a1a067803bf719922054c7434cde5a964
Carl Timmer <timmer@jlab.org>
2020-07-24 13:03:19 -0400

    add license file, properly

--- src/hipo/EvioXMLDictionary.cpp
+++ src/hipo/EvioXMLDictionary.cpp
@@ -1,8 +1,15 @@
 //
-// Created by Carl Timmer on 6/18/20.
+// Copyright 2020, Jefferson Science Associates, LLC.
+// Subject to the terms in the LICENSE file found in the top-level directory.
 //
+// EPSCI Group
+// Thomas Jefferson National Accelerator Facility
+// 12000, Jefferson Ave, Newport News, VA 23606
+// (757)-269-7100
+
 
 #include "EvioXMLDictionary.h"
+
 
 namespace evio {
 
