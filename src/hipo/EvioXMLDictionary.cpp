a24d905b37a3e717b51af2a8a63273fe6357d03c
Carl Timmer <timmer@jlab.org>
2020-07-27 14:28:20 -0400

    fix bug setting regex variable,  doc

--- src/hipo/EvioXMLDictionary.cpp
+++ src/hipo/EvioXMLDictionary.cpp
@@ -13,38 +13,42 @@
 
 namespace evio {
 
-    /** Element containing entire dictionary. */
+    // Element containing entire dictionary
     std::string const EvioXMLDictionary::DICT_TOP_LEVEL = "xmlDict";
 
-    /** There is only one type of element which directly defines an entry (strange name). */
+    // There is only one type of element which directly defines an entry (strange name)
     std::string const EvioXMLDictionary::ENTRY = "xmldumpDictEntry";
 
-    /** New, alternate, shortened form of ENTRY. */
+    // New, alternate, shortened form of ENTRY
     std::string const EvioXMLDictionary::ENTRY_ALT = "dictEntry";
 
-    /** Hierarchical container element. */
+    // Hierarchical container element
     std::string const EvioXMLDictionary::ENTRY_BANK = "bank";
 
-    /** Hierarchical leaf element. */
+    // Hierarchical leaf element
     std::string const EvioXMLDictionary::ENTRY_LEAF = "leaf";
 
-    /** Description element. */
+    // Description element
     std::string const EvioXMLDictionary::DESCRIPTION = "description";
 
-    /** The "format" attribute string. */
+    // The "format" attribute string
     std::string const EvioXMLDictionary::FORMAT = "format";
 
-    /** The "type" attribute string. */
+    // The "type" attribute string
     std::string const EvioXMLDictionary::TYPE = "type";
 
-    /** The "name" attribute string. */
+    // The "name" attribute string
     std::string const EvioXMLDictionary::NAME = "name";
 
-    /** The "tag" attribute string. */
+    // The "tag" attribute string
     std::string const EvioXMLDictionary::TAG = "tag";
 
-    /** The "num" attribute string. */
+    // The "num" attribute string
     std::string const EvioXMLDictionary::NUM = "num";
+
+
+    // Regular expression to parse tag & num
+    std::regex EvioXMLDictionary::pattern_regex("(\\d+)([ ]*-[ ]*(\\d+))?");
 
 
     /////////////////////////////////////////////////////
@@ -110,8 +114,6 @@ namespace evio {
      * @throws EvioException if parsing error.
      */
     void EvioXMLDictionary::parseXML(pugi::xml_parse_result & domDocument) {
-
-        pattern_regex = ("(\\d+)([ ]*-[ ]*(\\d+))?");
 
         pugi::xml_node topNode = topLevelDoc = doc.child(DICT_TOP_LEVEL.c_str());
 
