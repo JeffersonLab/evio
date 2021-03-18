5d89b11613a0c4fa446eb3528551cb3d34dc00f3
Carl Timmer <timmer@jlab.org>
2021-03-18 15:29:13 -0400

    try for regex linux compatibility

--- src/libsrc/Util.h
+++ src/libsrc/Util.h
@@ -1043,7 +1043,7 @@ namespace evio {
             while ( std::regex_search(text, match, env) ) {
                 const char * s = getenv(match[1].str().c_str());
                 const std::string var(s == nullptr ? "" : s);
-                text.replace(match[0].first, match[0].second, var.c_str());
+                text.replace(match[0].first, match[0].second, var.c_str(), 7);
             }
         }
 
