ea06451cac9c1d1128fa92ab0996f2ebc0e4c1ad
carltimmer <carl.timmer.1@gmail.com>
2024-11-08 16:16:11 -0500

    change to be the same as c++ EvioCompStructHandlerTest

--- java/org/jlab/coda/jevio/test/CompactStructureHandlerTest.java
+++ java/org/jlab/coda/jevio/test/CompactStructureHandlerTest.java
@@ -39,6 +39,9 @@ public class CompactStructureHandlerTest {
         int[] intData3 = new int[1];
         Arrays.fill(intData3, 9);
 
+        int[] intData4 = new int[1];
+        Arrays.fill(intData3, 10);
+
         try {
 
             // Build event (bank of banks) with EventBuilder object
@@ -47,7 +50,7 @@ public class CompactStructureHandlerTest {
 
             // bank of ints
             EvioBank bankInts = new EvioBank(tag+1, DataType.INT32, 2);
-            bankInts.appendIntData(intData1);
+            builder.setIntData(bankInts, intData1);
             builder.addChild(event, bankInts);
 
             // bank of banks
@@ -56,13 +59,18 @@ public class CompactStructureHandlerTest {
 
                 // bank of ints
                 EvioBank bankInts2 = new EvioBank(tag+19, DataType.INT32, 20);
-                bankInts2.appendIntData(intData2);
+                builder.setIntData(bankInts2, intData2);
                 builder.addChild(bankBanks, bankInts2);
 
             // bank of ints
             EvioBank bankInts3 = new EvioBank(tag+3, DataType.INT32, 4);
-            bankInts3.appendIntData(intData3);
+            builder.setIntData(bankInts3, intData3);
             builder.addChild(event, bankInts3);
+
+            // bank of ints
+            EvioBank bankInts4 = new EvioBank(tag+5, DataType.INT32, 5);
+            builder.setIntData(bankInts4, intData4);
+            builder.addChild(event, bankInts4);
 
         }
         catch (EvioException e) {
@@ -70,29 +78,15 @@ public class CompactStructureHandlerTest {
         }
 
         return event;
-
     }
 
-
-
-    static ByteBuffer createBuffer(int tag, int num) {
-
-         EvioEvent bank = createSingleEvent(tag);
-         int byteSize = bank.getTotalBytes();
-
-         ByteBuffer bb = ByteBuffer.allocate(byteSize);
-         bank.write(bb);
-         bb.flip();
-
-         return bb;
-    }
 
 
     static ByteBuffer createAddBuffer(int tag, int num) {
 
         ByteBuffer bb = null;
         try {
-            CompactEventBuilder builder = new CompactEventBuilder(4*5, ByteOrder.BIG_ENDIAN);
+            CompactEventBuilder builder = new CompactEventBuilder(4*5, ByteOrder.LITTLE_ENDIAN);
             builder.openBank(tag, num, DataType.BANK);
             builder.openBank(tag+1, num+1, DataType.INT32);
             builder.addIntData(new int[] {6});
@@ -113,40 +107,63 @@ public class CompactStructureHandlerTest {
     public static void main(String args[]) {
 
         try {
+            int tag = 1;
+            int num = 1;
+
             ByteBuffer buf = ByteBuffer.allocate(1024);
-            EvioEvent event = createSingleEvent(1);
+            buf.order(ByteOrder.LITTLE_ENDIAN);
+            EvioEvent event = createSingleEvent(tag);
+
+            // Evio 6 format (buf cleared (pos -> 0, lim->cap) before writing.
+            // Note: header = 14 int + 1 int / event (index) = 4*(14 + 1) bytes = 60 bytes.
+            // For evio 6, position just past header = 60 (1 event in buf)
             EventWriter writer = new EventWriter(buf);
             writer.writeEvent(event);
+            writer.close();
 
-            System.out.println("After writer, buf pos = " + buf.position() +
-                               ", lim = " + buf.limit());
+            ByteBuffer finishedBuf = writer.getByteBuffer();
 
-            // Position buffer to after block header
-            buf.limit(32 + 4*13).position(32);
-            Utilities.printBuffer(buf, 0, 29, "EVENT");
+            Utilities.printBytes(finishedBuf, 0, finishedBuf.limit(), "Finished Buffer");
+
+            buf.limit(60 + 4*16).position(60);
+            Utilities.printBytes(buf, 60, 4*16, "Full Event");
 
             EvioCompactStructureHandler handler =
                     new EvioCompactStructureHandler(buf, DataType.BANK);
             List<EvioNode> list = handler.getNodes();
 
-            // Remove  node
-            handler.removeStructure(list.get(4));
+            // Remove last node
+            handler.removeStructure(list.get(list.size() - 1));
 
-            // buffer to add
-            ByteBuffer bb = createAddBuffer(6,6);
+            // Add buffer containing event
+            ByteBuffer bb = createAddBuffer(tag+4,tag+4);
             ByteBuffer newBuf = handler.addStructure(bb);
 
-            Utilities.printBuffer(newBuf, 0, 29, "New event");
+            Utilities.printBytes(bb, 0, bb.limit(), "New event");
+
+
+            // Search event for tag = 2, num = 2
+            List<EvioNode> foundList = handler.searchStructure(2, 2);
+            for (EvioNode n: foundList) {
+                // Look at each node (structure)
+                System.out.println("Found struct with tag = 2, num = 2");
+                Utilities.printBytes(n, n.getTotalBytes(), "Found node");
+            }
+
 
             list = handler.getNodes();
             System.out.println("Got " + list.size() + " nodes after everything");
             int i=0;
             for (EvioNode n : list) {
-                // Look at data for 3rd node
-                ByteBuffer dd = handler.getStructureBuffer(list.get(i));
-                Utilities.printBuffer(dd, 0, 29, "Struct buf for node " + (++i));
-            }
+                // Look at each node (structure)
+                ByteBuffer dat = handler.getStructureBuffer(list.get(i));
+                Utilities.printBytes(dat, dat.position(), dat.limit(), "Struct for node " + (i + 1));
 
+                // Look at data for each node (structure)
+                ByteBuffer dd = handler.getData(list.get(i));
+                Utilities.printBytes(dd, dd.position(), dd.limit(), "Data for node " + (i + 1));
+                i++;
+            }
         }
         catch (EvioException e) {
             e.printStackTrace();
