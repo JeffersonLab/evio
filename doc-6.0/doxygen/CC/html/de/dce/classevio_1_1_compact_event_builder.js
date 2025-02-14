c9380b708399fea7c927f7c606c88491967bb17d
Carl Timmer <timmer@jlab.org>
2025-02-14 15:03:13 +0000

    Update docs

--- doc-6.0/doxygen/CC/html/de/dce/classevio_1_1_compact_event_builder.js
+++ doc-6.0/doxygen/CC/html/de/dce/classevio_1_1_compact_event_builder.js
@@ -1,12 +1,12 @@
 var classevio_1_1_compact_event_builder =
 [
     [ "CompactEventBuilder", "de/dce/classevio_1_1_compact_event_builder.html#afba69414ba4596c97c7abfd180186a25", null ],
-    [ "CompactEventBuilder", "de/dce/classevio_1_1_compact_event_builder.html#acfd2ada6c368910006f3981e982604df", null ],
+    [ "CompactEventBuilder", "de/dce/classevio_1_1_compact_event_builder.html#ad99d3c5d21f5cd66fb54230fe10afad8", null ],
     [ "addByteData", "de/dce/classevio_1_1_compact_event_builder.html#a897f62ae51a09cff4a05bb5320e86422", null ],
     [ "addCharData", "de/dce/classevio_1_1_compact_event_builder.html#ac7ca38017f7d4087831f05569d559910", null ],
     [ "addCompositeData", "de/dce/classevio_1_1_compact_event_builder.html#aa9b4794a8a205b1e96fe7226cef3c864", null ],
     [ "addDoubleData", "de/dce/classevio_1_1_compact_event_builder.html#a352207f5c6fb728773649bc44f17adca", null ],
-    [ "addEvioNode", "de/dce/classevio_1_1_compact_event_builder.html#a269b8a8d59188c70e793f0c0eeb95615", null ],
+    [ "addEvioNode", "de/dce/classevio_1_1_compact_event_builder.html#a564284bcbfbd453eaaba503b75e83771", null ],
     [ "addFloatData", "de/dce/classevio_1_1_compact_event_builder.html#add132a161db8b8cc578203233454df7f", null ],
     [ "addIntData", "de/dce/classevio_1_1_compact_event_builder.html#a0dbbefe7f192fda740b6827184f18475", null ],
     [ "addLongData", "de/dce/classevio_1_1_compact_event_builder.html#a9ed7c1b0d37bf484c65bfa7bfbec4bad", null ],
@@ -19,12 +19,12 @@ var classevio_1_1_compact_event_builder =
     [ "addUShortData", "de/dce/classevio_1_1_compact_event_builder.html#aadfc320edcf4680e0932274ff26e00c2", null ],
     [ "closeAll", "de/dce/classevio_1_1_compact_event_builder.html#a6102f05fbcc1e84fdc423e1c24dfc1de", null ],
     [ "closeStructure", "de/dce/classevio_1_1_compact_event_builder.html#a1c1a628643059987a3a8371ce794c4af", null ],
-    [ "getBuffer", "de/dce/classevio_1_1_compact_event_builder.html#a38e1d5ab9c5a4bf799d60c5a1d216d83", null ],
+    [ "getBuffer", "de/dce/classevio_1_1_compact_event_builder.html#a8dc8c736c49ec88c52e6b1fe62e8cd4e", null ],
     [ "getTotalBytes", "de/dce/classevio_1_1_compact_event_builder.html#a43f3bdca4a61710c6b903c9e926042b3", null ],
     [ "openBank", "de/dce/classevio_1_1_compact_event_builder.html#a14cd5da1c84c052c8a446235dd67f03c", null ],
     [ "openSegment", "de/dce/classevio_1_1_compact_event_builder.html#a565d005085544e6e8c66e410fd1e8a8a", null ],
     [ "openTagSegment", "de/dce/classevio_1_1_compact_event_builder.html#a9b7812e1d18349d5fec68cd1611dfcf8", null ],
-    [ "setBuffer", "de/dce/classevio_1_1_compact_event_builder.html#aea41d8c743a988976ab75ce42b368b9a", null ],
+    [ "setBuffer", "de/dce/classevio_1_1_compact_event_builder.html#a73305ba781c49f0c0367ac823a8a6135", null ],
     [ "setTopLevelTag", "de/dce/classevio_1_1_compact_event_builder.html#a01e1b344efd2d7f26fe331c241175822", null ],
     [ "toFile", "de/dce/classevio_1_1_compact_event_builder.html#a9042fd06e8d738ef387684b6df3c765f", null ]
 ];
