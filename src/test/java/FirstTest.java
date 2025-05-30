a6a6125d42f25bea7b52139a0272e0cbec1cfd72
Jon Zarling <jonzarling@gmail.com>
2025-05-30 16:52:37 -0500

    Add JUnit dependency and a first dummy example (borrowed from junit5-samples)

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,25 @@
+/*
+ *   Copyright (c) 2025.  Jefferson Lab (JLab). All rights reserved. Permission
+ *   to use, copy, modify, and distribute  this software and its documentation for
+ *   educational, research, and not-for-profit purposes, without fee and without a
+ *   signed licensing agreement.
+ */
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+@Tag("fast")
+class FirstTest {
+
+	@Test
+	@DisplayName("My 1st JUnit 5 test! 😎")
+	void myFirstTest(TestInfo testInfo) {
+		assertEquals(2, 1+1, "1 + 1 should equal 2");
+		assertEquals("My 1st JUnit 5 test! 😎", testInfo.getDisplayName(), () -> "TestInfo is injected correctly");
+	}
+
+}
